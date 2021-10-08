520d2c2373258970d5a9458f6f325c2bbf782fe0
Ji-Hun Jeon <nohack-@naver.com>
2021-10-08 16:56:12 +0900

    Promise 추가 공부

--- javascript/2021/10/08/promise.js
+++ javascript/2021/10/08/promise.js
@@ -26,3 +26,79 @@ request
   })
   .then((response) => response.json())
   .then(console.log);
+
+// Promise example
+class UserStorage {
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (id === 'nohack' && password === '123123') {
+          resolve(id);
+        } else {
+          reject(new Error('not found'));
+        }
+      }, 2000);
+    });
+  }
+
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === 'nohack') {
+          resolve({ name: 'nohack', role: 'admin' });
+        } else {
+          reject(new Error('no access'));
+        }
+      }, 1000);
+    });
+  }
+}
+
+// Class 코드는 동일합니다.
+const userStorage = new UserStorage();
+
+try {
+  const userId = await userStorage.loginUser('nohack', '123123');
+  const userWithRole = await userStorage.getRoles(userId);
+  // { name: 'nohack', role: 'admin' }
+  console.log(userWithRole);
+} catch (error) {
+  console.error(error);
+}
+
+// userStorage.loginUser(
+//   'nohack',
+//   '123123',
+//   (userId) => {
+//     userStorage.getRoles(
+//       userId,
+//       (userWithRole) => {
+//         console.log(userWithRole);
+//       },
+//       (error) => console.error(error)
+//     );
+//   },
+//   (error) => console.error(error)
+// );
+
+// { name: 'nohack', role: 'admin' }
+// const userStorage = new UserStorage();
+// userStorage
+//   .loginUser('nohack', '123123')
+//   .then(userStorage.getRoles)
+//   .then(console.log)
+//   .catch(console.error);
+
+// const checkUser = (userId) =>
+//   new Promise((resolve, reject) => {
+//     if (userId === 'nohack') {
+//       resolve('success');
+//     } else {
+//       reject('failure');
+//     }
+//   });
+
+// // success를 출력
+// checkUser('nohack')
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error));
