13e37b528d4c158af58fe4608aecadb2a41064b3
n0hack <nohack-@naver.com>
2021-10-31 16:44:01 +0900

    Pagina Web

---
+++ 2021/10/31/pagina-web/assets/js/main.js
@@ -0,0 +1,11 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("menu-toggle", "nav-menu");
