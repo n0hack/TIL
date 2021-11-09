f06ba54714fd24359074d91637235d903e0c6c60
n0hack <nohack-@naver.com>
2021-11-10 03:40:40 +0900

    리액트를 다루는 기술 [09. 컴포넌트 스타일링]

---
+++ from-books/react-velopert/09-styles/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SassComponent from './SassComponent';
+
+function App() {
+  return (
+    <>
+      <SassComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ from-books/react-velopert/09-styles/src/SassComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SassComponent.scss';
+
+const SassComponent = () => {
+  return (
+    <div className="SassComponent">
+      <div className="box red"></div>
+      <div className="box orange"></div>
+      <div className="box yellow"></div>
+      <div className="box green"></div>
+      <div className="box blue"></div>
+      <div className="box indigo"></div>
+      <div className="box violet"></div>
+    </div>
+  );
+};
+
+export default SassComponent;
