94e133fd118cfd1b25eb9379e4f04b024f89b901
n0hack <nohack-@naver.com>
2021-12-01 12:41:06 +0900

    호이스팅 (Hoisting)

---
+++ from-books/js-deep-dive/hoisting.js
@@ -0,0 +1,13 @@
+console.log(var변수);
+console.log(let변수);
+// console.log(const상수);
+// console.log(클래스);
+console.log(함수);
+console.log(제너레이터);
+
+var var변수 = 1;
+// let let변수 = 2;
+const const상수 = 3;
+class 클래스 {}
+function 함수() {}
+function* 제너레이터() {}
