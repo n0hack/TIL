348cdfd57e1f1c54fb5fa99cd68924184f98fa9a
n0hack <nohack-@naver.com>
2022-01-13 16:21:15 +0900

    04. 자바스크립트 기초 - 타입 변환과 함수 (퀴즈)

---
+++ from-books/기초부터-완성까지-프런트엔드/04/quiz.js
@@ -0,0 +1,7 @@
+console.log(null == undefined); // true
+console.log(false == 0); // true
+console.log(0 == []); // true?
+console.log(String([1, 2, 3])); // '[1,2,3]'
+console.log('a' + {}); // 'a[object Object]'
+console.log('1' < ['02']); // false
+console.log(NaN == NaN); // false
