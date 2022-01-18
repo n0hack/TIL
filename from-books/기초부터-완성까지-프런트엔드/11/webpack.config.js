642d15243e3be43da8a6020e5e4ffda3309225b3
n0hack <nohack-@naver.com>
2022-01-18 20:01:51 +0900

    11. 프런트엔드 개발 도구

---
+++ from-books/기초부터-완성까지-프런트엔드/11/webpack.config.js
@@ -0,0 +1,6 @@
+const devConfig = require('./webpack.dev.config');
+const prodConfig = require('./webpack.prod.config');
+
+module.exports = (env, { mode }) => {
+  return mode === 'production' ? prodConfig : devConfig;
+};
