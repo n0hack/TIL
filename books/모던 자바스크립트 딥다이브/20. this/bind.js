3485dede61364b65da44f1400ecdea86d4742a86
n0hack <nohack-@naver.com>
2022-02-12 02:38:00 +0900

    콜백 함수에 직접 바인딩 테스트

--- books/모던 자바스크립트 딥다이브/20. this/bind.js
+++ books/모던 자바스크립트 딥다이브/20. this/bind.js
@@ -5,10 +5,13 @@ const obj = {
     console.log("foo's this: ", this);
     // 브라우저에서는 일반 함수로 호출하면 전역 객체를 가리킴
     // Node.js는 Timeout 스코프를 가리키는 듯?
-    setTimeout(function () {
-      console.log("callback's this: ", this);
-      console.log("callback's this.value: ", this.value);
-    }, 100);
+    setTimeout(
+      function () {
+        console.log("callback's this: ", this);
+        console.log("callback's this.value: ", this.value);
+      }.bind(this),
+      100
+    );
   },
 };
 obj.foo();
