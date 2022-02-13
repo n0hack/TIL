0d1936684735ccad7bba55b9b5b917af8a16208a
n0hack <nohack-@naver.com>
2022-02-14 07:03:51 +0900

    Promise.all

--- books/모던 자바스크립트 딥다이브/45. 프로미스/all.js
+++ books/모던 자바스크립트 딥다이브/45. 프로미스/all.js
@@ -1,21 +1,41 @@
-const requestData1 = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(1);
-    }, 3000)
-  );
+const promiseGet = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
 
-const requestData2 = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(2);
-    }, 2000)
-  );
-const requestData3 = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(3);
-    }, 1000)
-  );
+    xhr.onload = () => {
+      if (xhr.status === 200) resolve(JSON.parse(xhr.response));
+      else reject(new Error(xhr.status));
+    };
+  });
+};
 
-Promise.all([requestData1(), requestData2(), requestData3()]).then(console.log);
+const githubIds = ['jeresig', 'ahejlsberg', 'ungmo2'];
+
+Promise.all(githubIds.map((id) => promiseGet(`https://api.github.com/${id}`)))
+  .then((users) => users.map((user) => user.name))
+  .then(console.log)
+  .catch(console.error);
+
+// const requestData1 = () =>
+//   new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve(1);
+//     }, 3000)
+//   );
+
+// const requestData2 = () =>
+//   new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve(2);
+//     }, 2000)
+//   );
+// const requestData3 = () =>
+//   new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve(3);
+//     }, 1000)
+//   );
+
+// Promise.all([requestData1(), requestData2(), requestData3()]).then(console.log);
