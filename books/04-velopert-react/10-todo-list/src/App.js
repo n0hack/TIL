9abfce54be52da9d0a076c54b46feb2833f1fa96
n0hack <nohack-@naver.com>
2022-03-27 02:01:58 +0900

    10. Todo List

---
+++ books/04-velopert-react/10-todo-list/src/App.js
@@ -0,0 +1,54 @@
+import { useCallback, useRef, useState } from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트의 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
+  ]);
+  // 렌더링 되는 정보가 아니므로 Ref를 사용해 내부적으로 사용
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      );
+    },
+    [todos]
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+}
+
+export default App;
---
+++ books/04-velopert-react/10-todo-list/src/components/TodoList.js
@@ -0,0 +1,19 @@
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
