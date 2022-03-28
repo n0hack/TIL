dbd1ae2b0095631f17efc9ba27ef171418acc7a4
n0hack <nohack-@naver.com>
2022-03-29 06:20:35 +0900

    18. 리덕스 미들웨어를 통한 비동기 작업 관리 (redux-saga)

--- books/04-velopert-react/18-redux-middleware-saga/src/App.js
+++ books/04-velopert-react/18-redux-middleware-saga/src/App.js
@@ -1,9 +1,11 @@
 import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
 
 function App() {
   return (
     <div className="App">
       <CounterContainer />
+      <SampleContainer />
     </div>
   );
 }
--- books/04-velopert-react/18-redux-middleware-saga/src/modules/index.js
+++ books/04-velopert-react/18-redux-middleware-saga/src/modules/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import counter, { counterSaga } from './counter';
+import sample, { sampleSaga } from './sample';
+import loading from './loading';
 
-const rootReducer = combineReducers({ counter });
+const rootReducer = combineReducers({ counter, sample, loading });
 
 export function* rootSaga() {
   // all은 사가들을 합쳐주는 역할
-  yield all([counterSaga()]);
+  yield all([counterSaga(), sampleSaga()]);
 }
 
 export default rootReducer;
