3f25d11980666bae0e2f149cb5e0491d04eacab4
n0hack <nohack-@naver.com>
2022-03-31 01:20:53 +0900

    react-cookie

--- docs/react/practice/src/App.js
+++ docs/react/practice/src/App.js
@@ -1,17 +1,12 @@
-import PComponent from './components/PComponent';
 import 'bootstrap/dist/css/bootstrap.css';
-import Reactstrap from './components/Reactstrap';
-import SweetAlert from './components/SweetAlert';
-import Currying from './components/Currying';
-import HOC from './components/HOC';
-import ContextFirst from './components/ContextFirst';
+import CookieSave from './components/CookieSave';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React v18</h1>
       <p>CSS 적용하기</p>
-      <ContextFirst />
+      <CookieSave />
     </div>
   );
 }
