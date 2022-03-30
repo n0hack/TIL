c13ef98764f0c4b40514423a4f5898fda26ae86a
n0hack <nohack-@naver.com>
2022-03-30 17:11:27 +0900

    sweetalert2 연습

--- docs/react/practice/src/App.js
+++ docs/react/practice/src/App.js
@@ -1,13 +1,14 @@
 import PComponent from './components/PComponent';
 import 'bootstrap/dist/css/bootstrap.css';
 import Reactstrap from './components/Reactstrap';
+import SweetAlert from './components/SweetAlert';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React v18</h1>
       <p>CSS 적용하기</p>
-      <Reactstrap />
+      <SweetAlert />
     </div>
   );
 }
