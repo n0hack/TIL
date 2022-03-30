8673d76bc7c85badeacdb2db0da6619ced194423
n0hack <nohack-@naver.com>
2022-03-30 20:45:42 +0900

    Currying 실습

--- docs/react/practice/src/App.js
+++ docs/react/practice/src/App.js
@@ -2,13 +2,14 @@ import PComponent from './components/PComponent';
 import 'bootstrap/dist/css/bootstrap.css';
 import Reactstrap from './components/Reactstrap';
 import SweetAlert from './components/SweetAlert';
+import Currying from './components/Currying';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React v18</h1>
       <p>CSS 적용하기</p>
-      <SweetAlert />
+      <Currying />
     </div>
   );
 }
