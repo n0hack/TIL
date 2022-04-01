37f7709a2985661b8ef9fe06c820ee0d46d7093c
n0hack <nohack-@naver.com>
2022-04-01 16:55:26 +0900

    kakao map api

--- docs/react/full/client/src/App.js
+++ docs/react/full/client/src/App.js
@@ -1,3 +1,4 @@
+/* global kakao */
 import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -10,6 +11,15 @@ function App() {
       console.log(json);
     };
     fn();
+
+    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    var options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
   }, []);
 
   const onSubmit = useCallback(
@@ -36,6 +46,7 @@ function App() {
 
   return (
     <div className="App">
+      <div id="map" style={{ width: '500px', height: '400px' }}></div>
       <form onSubmit={onSubmit}>
         <input
           type="text"
