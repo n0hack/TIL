46538a82a69315ff2943fe2ea0aa4466c1a0e7ab
n0hack <nohack-@naver.com>
2022-04-02 00:59:15 +0900

    kakao.maps 커스텀 오버레이

--- docs/react/full/client/src/App.js
+++ docs/react/full/client/src/App.js
@@ -1,25 +1,130 @@
 /* global kakao */
 import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
+import jsonData from './map_info.json';
+import $ from 'jquery';
 
 function App() {
   const [username, setUsername] = useState('');
   useEffect(() => {
-    const fn = async () => {
-      const response = await fetch('/users');
-      const json = await response.json();
-      console.log(json);
+    // const fn = async () => {
+    //   const response = await fetch('/users');
+    //   const json = await response.json();
+    //   console.log(json);
+    // };
+    // fn();
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
     };
-    fn();
+    const map = new kakao.maps.Map(container, options);
+    const clusterer = new kakao.maps.MarkerClusterer({
+      map: map,
+      averageCenter: true,
+      minLevel: 5,
+    });
 
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+    const markers = $(jsonData.positions).map(function (i, position) {
+      let imgSrc =
+        position.finish === '완치'
+          ? require(`./img/clearp0${i + 1}.png`)
+          : require(`./img/hop0${i + 1}.png`);
 
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      const imgSize = new kakao.maps.Size(60, 70);
+      const imgOption = { offset: new kakao.maps.Point(27, 69) };
+      const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
+
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(position.lat, position.lng),
+        image: markerImg,
+      });
+
+      var content =
+        '<div class="overlaybox">' +
+        '    <div class="boxtitle">' +
+        position.num +
+        '번째 확진자</div>' +
+        '    <div class="boxtitle2" style="margin-left: 150px;"><font color="white"">닫기 버튼 [X]</font></div>' +
+        '    <div class="first first_' +
+        position.num +
+        '">' +
+        '        <div class="triangle text">' +
+        position.num +
+        '</div>' +
+        '        <div class="movietitle text">' +
+        position.comment +
+        '</div>' +
+        '    </div>' +
+        '    <ul style = "overflow:scroll">' +
+        '        <li class="up">' +
+        '            <span class="number">격리:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.date +
+        '</span>' +
+        '            <span class="number">완치:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.finish_date +
+        '</span>' +
+        '        </li>' +
+        '        <li class="up">' +
+        '            <span class="number">성별:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.gender +
+        '</span>' +
+        '            <span class="number">생년:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.birth +
+        '</span>' +
+        '            <span class="number">국적:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.nation +
+        '</span>' +
+        '        </li>' +
+        '        <li class="up">' +
+        '            <span class="number">접속자수:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.contact +
+        '</span>' +
+        '            <span class="number">격리조치중:</span>' +
+        '            <span class="title" style="font-weight:bold;color:white">' +
+        position.Isolation +
+        '</span>' +
+        '        </li>' +
+        '    </ul>' +
+        '</div>';
+
+      var lat = Number(position.lat);
+      var lng = Number(position.lng);
+      var lat_string = lat.toString();
+      var lng_string = lng.toString();
+
+      var customOverlay = new kakao.maps.CustomOverlay({
+        position: new kakao.maps.LatLng(lat_string, lng_string),
+        content: content,
+        xAnchor: 0.25,
+        yAnchor: 0.95,
+      });
+
+      var clickHandler1 = function (event) {
+        customOverlay.setMap(map);
+
+        $('.boxtitle2').click(function () {
+          customOverlay.setMap(null);
+        });
+
+        $('.first_' + position.num).css({
+          background: 'url(' + position.hospi_img + ')',
+          'background-size': '247px 247px',
+        });
+      };
+
+      kakao.maps.event.addListener(marker, 'click', clickHandler1);
+      return marker;
+    });
+
+    clusterer.addMarkers(markers);
   }, []);
 
   const onSubmit = useCallback(
