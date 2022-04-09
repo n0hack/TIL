404036779e145bf7ddf99e496170df9017fdc934
n0hack <nohack-@naver.com>
2022-04-10 06:10:41 +0900

    filter 추가

--- books/05-functional-programming/video/app.js
+++ books/05-functional-programming/video/app.js
@@ -1,15 +1,20 @@
 /* 유인동 CTO님 강의 */
 const log = console.log;
 
+/* 조건 처리: filter 함수 */
+function* filter(f, list) {
+  for (const a of list) {
+    if (f(a)) yield a;
+  }
+}
+
 // 리스트에서 홀수를 length만큼 뽑아서 제곱한 후 모두 더하기
 function f(list, length) {
   let i = 0;
   let acc = 0;
-  for (const a of list) {
-    if (a % 2) {
-      acc = acc + a * a;
-      if (++i === length) break;
-    }
+  for (const a of filter((x) => x % 2, list)) {
+    acc = acc + a * a;
+    if (++i === length) break;
   }
   log(acc);
 }
