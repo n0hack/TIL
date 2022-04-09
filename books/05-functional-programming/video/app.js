e8ba302097b502bfa97cb8b015ef24614b59c955
n0hack <nohack-@naver.com>
2022-04-10 07:49:05 +0900

    users 배열을 통한 실무 예시

--- books/05-functional-programming/video/app.js
+++ books/05-functional-programming/video/app.js
@@ -133,3 +133,56 @@ go(
   reduce(add),
   log
 );
+
+/* 유저 목록 : 실무 예시 */
+const users = [
+  {
+    name: 'a',
+    age: 21,
+    family: [
+      { name: 'a1', age: 53 },
+      { name: 'a2', age: 47 },
+      { name: 'a3', age: 16 },
+      { name: 'a4', age: 14 },
+    ],
+  },
+  {
+    name: 'b',
+    age: 24,
+    family: [
+      { name: 'b1', age: 58 },
+      { name: 'b2', age: 51 },
+      { name: 'b3', age: 10 },
+      { name: 'b4', age: 22 },
+    ],
+  },
+  {
+    name: 'c',
+    age: 31,
+    family: [
+      { name: 'c1', age: 64 },
+      { name: 'c2', age: 62 },
+    ],
+  },
+  {
+    name: 'd',
+    age: 20,
+    family: [
+      { name: 'd1', age: 42 },
+      { name: 'd2', age: 42 },
+      { name: 'd3', age: 11 },
+      { name: 'd4', age: 7 },
+    ],
+  },
+];
+
+go(
+  users,
+  L.map((u) => u.family),
+  L.flat,
+  L.filter((u) => u.age < 20),
+  L.map((u) => u.age),
+  take(2),
+  reduce(add),
+  log
+);
