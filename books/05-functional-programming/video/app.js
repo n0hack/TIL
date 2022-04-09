befbdd8f29a814567abeef059adc8dc6d8f1a0a1
n0hack <nohack-@naver.com>
2022-04-10 06:12:42 +0900

    map 추가

--- books/05-functional-programming/video/app.js
+++ books/05-functional-programming/video/app.js
@@ -8,12 +8,22 @@ function* filter(f, list) {
   }
 }
 
+/* 연산 처리: map 함수 */
+function* map(f, list) {
+  for (const a of list) {
+    yield f(a);
+  }
+}
+
 // 리스트에서 홀수를 length만큼 뽑아서 제곱한 후 모두 더하기
 function f(list, length) {
   let i = 0;
   let acc = 0;
-  for (const a of filter((x) => x % 2, list)) {
-    acc = acc + a * a;
+  for (const a of map(
+    (x) => x ** 2,
+    filter((x) => x % 2, list)
+  )) {
+    acc = acc + a;
     if (++i === length) break;
   }
   log(acc);
