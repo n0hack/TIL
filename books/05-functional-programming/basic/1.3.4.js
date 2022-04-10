4e0d944b85774112a2c8231f642bf7021e5d6cbf
n0hack <nohack-@naver.com>
2022-04-10 21:23:33 +0900

    함수 합성 구현 (_.compose)

--- books/05-functional-programming/basic/1.3.4.js
+++ books/05-functional-programming/basic/1.3.4.js
@@ -48,14 +48,31 @@ const beq = (a) => (b) => a === b;
 const positive = (list) => _.find(list, _.identify);
 const negative = (list) => _.findIndex(list, _.falsy);
 
-/* 리스트의 아이템 중 하나라도 참이라면 true */
-_.some = function (list) {
-  return not(not(positive(list)));
+_.compose = function () {
+  const args = arguments;
+  const start = arguments.length - 1;
+
+  return function () {
+    let i = start;
+    let result = args[start].apply(this, arguments);
+    while (i--) result = args[i].call(this, result);
+    return result;
+  };
 };
 
+const greet = (name) => `hi, ${name}`;
+const exclaim = (statement) => `${statement.toUpperCase()}!`;
+
+/* 리스트의 아이템 중 하나라도 참이라면 true */
+/* _.some = function (list) {
+  return not(not(positive(list)));
+}; */
+_.some = _.compose(not, not, positive);
+
 /* 리스트의 아이템 모두 참이라면 true */
-_.every = function (list) {
+/* _.every = function (list) {
   // 하나라도 거짓이라면 거짓인데, filter는 모두 순회하기에 비효율적
   // return _.filter(list, _.identify).length === list.length;
   return beq(-1)(negative(list));
-};
+}; */
+_.every = _.compose(beq(-1), negative);
