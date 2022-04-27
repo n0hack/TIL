64b045704dc88349b18acb756ef286269cd586e6
n0hack <nohack-@naver.com>
2022-04-27 20:20:42 +0900

    [syntax] 데코레이터

--- docs/typescript/syntax/src/decorator.ts
+++ docs/typescript/syntax/src/decorator.ts
@@ -1,8 +1,10 @@
-// 데코레이터는 런타임에 호출?
+// 반환 타입이 void인 경우 클래스를 수정하지 않고, 읽기만 함
 function whoAmI(target: Function): void {
-  console.log(`You are:\n${target}`);
+  console.log(`You are: \n${target}`);
 }
 
+// tsc로 변환 중 데코레이터가 적용되었는지 평가하고,
+// 런타임 시 실행될 자바스크립트가 생성됨
 @whoAmI
 class Friend {
   constructor(private name: string, private age: number) {}
@@ -11,38 +13,42 @@ class Friend {
 function UIComponent(html: string) {
   console.log(`The decorator received ${html}\n`);
   return function (target: Function) {
-    console.log(`Someone wants to create a UI Compoentn from\n${target}`);
+    console.log(`Someone wants to create a UI component from\n${target}`);
   };
 }
 
-@UIComponent('<h1>HEllo Shopper!</h1>')
+@UIComponent('<h1>Hello</h1>')
 class Shopper {
   constructor(private name: string) {}
 }
 
-type constructorMixin = { new (...args: any[]): {} };
+console.log();
+console.log();
 
+type ConstructorMixin = { new (...args: any[]): {} };
+// 믹스인(Mixin): 클래스를 변경하는 데코레이터
 function useSalutation(salutation: string) {
-  return function <T extends constructorMixin>(target: T) {
+  // 팩토리
+  return function <T extends ConstructorMixin>(target: T) {
     return class extends target {
       name: string;
-      private message: string;
+      message: string = `${salutation}`;
       sayHello() {
-        this.message = this.name;
-        console.log(this);
+        console.log(`Hello ${this.message}${this.name}`);
       }
     };
   };
 }
 
-@useSalutation('Mr. ')
+@useSalutation('Mr.')
 class Greeter {
   constructor(public name: string) {}
   sayHello() {
     console.log(`Hello ${this.name}`);
   }
 }
-const grt = new Greeter('John');
+
+const grt = new Greeter('NoHack');
 grt.sayHello();
 
 class Trade {
@@ -53,19 +59,19 @@ class Trade {
     operation: string,
     traderID: number
   ) {
-    console.log('called');
+    console.log(arguments);
   }
 }
-const trade = new Trade();
-trade.placeOrder('IBM', 100, 'Buy', 123);
 
 function logTrade(target, key, descriptor) {
-  // 원본 메서드 저장
   const originalCode = descriptor.value;
-  // 메서드 수정
   descriptor.value = function () {
     console.log(`Invoked ${key} providing:`, arguments);
-    return originalCode.apply(this, arguments);
+    originalCode.apply(this, arguments);
   };
   return descriptor;
 }
+
+const trade = new Trade();
+trade.placeOrder('IBM', 100, 'Buy', 123);
+trade.placeOrder('NoHack', 123, 'zz', 1111);
