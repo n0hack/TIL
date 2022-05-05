6b4d0a0e91a7d3d7b29ac2784ccacee7e9151f02
n0hack <nohack-@naver.com>
2022-05-05 20:36:51 +0900

    5.3: 리액트 부트스트랩

---
+++ books/02-react-style-guide/05-components-bootstrap/src/App.js
@@ -0,0 +1,13 @@
+import ButtonComponent from './components/ButtonComponent';
+import DropdownComponent from './components/DropdownComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <DropdownComponent />
+      <ButtonComponent />
+    </div>
+  );
+}
+
+export default App;
