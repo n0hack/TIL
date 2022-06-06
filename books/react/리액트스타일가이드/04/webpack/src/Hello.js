7baea193a20dd5d589cea7af737caecba903c803
n0hack <nohack-@naver.com>
2022-06-07 00:29:53 +0900

    웹팩 구성 (dev-server, hmr)

--- books/react/리액트스타일가이드/04/webpack/src/Hello.js
+++ books/react/리액트스타일가이드/04/webpack/src/Hello.js
@@ -2,7 +2,7 @@ import React from "react";
 import { hot } from "react-hot-loader";
 
 function Hello() {
-  return <div>Hello World</div>;
+  return <div>Hello World!</div>;
 }
 
 export default hot(module)(Hello);
