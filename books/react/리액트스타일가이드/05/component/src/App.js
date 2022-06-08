91371bea6b584a2ba0fcde1d6237c77985234606
n0hack <nohack-@naver.com>
2022-06-08 10:34:17 +0900

    컴포넌트

---
+++ books/react/리액트스타일가이드/05/component/src/App.js
@@ -0,0 +1,12 @@
+import UserCardWrapper from "./components/UserCardWrapper";
+
+function App() {
+  // 11b
+  return (
+    <div className="App">
+      <UserCardWrapper />
+    </div>
+  );
+}
+
+export default App;
