438d921851b3d14169f49d0b544313b7d4a0230a
n0hack <nohack-@naver.com>
2022-06-12 08:40:42 +0900

    eject

--- books/react/리액트스타일가이드/06/css-in-js/src/App.tsx
+++ books/react/리액트스타일가이드/06/css-in-js/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./Hello";
 
 function App() {
   return (
     <div className="App">
+      <Hello />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ books/react/리액트스타일가이드/06/css-in-js/src/Hello.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import RedColor from "./atoms/Color";
+import styles from "./Hello.module.css";
+
+function Hello() {
+  return <div className={styles.hello}>Hello, world!</div>;
+}
+
+export default Hello;
