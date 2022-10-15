19cb50b2d880bdb51b588c1210f4f06258e4e556
n0hack <nohack-@naver.com>
2022-10-16 00:53:23 +0900

    [feat] 버튼 size prop + story 추가

--- 2022/react/storybook-design-system/src/Button/Button.stories.tsx
+++ 2022/react/storybook-design-system/src/Button/Button.stories.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Button from './Button';
+import { css } from '@emotion/react';
 
 export default {
   title: 'components/Button',
   component: Button,
+  argTypes: {
+    children: { description: '버튼 안의 내용' },
+    theme: { description: '버튼의 생김새를 설정합니다.' },
+    size: { description: '버튼의 크기를 설정합니다.' },
+    onClick: { description: '클릭했을 때 호출할 함수' },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
@@ -12,4 +19,50 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   children: <React.Fragment>BUTTON</React.Fragment>,
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: <React.Fragment>PRIMARY</React.Fragment>,
+  theme: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: <React.Fragment>PRIMARY</React.Fragment>,
+  theme: 'secondary',
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  children: <React.Fragment>PRIMARY</React.Fragment>,
+  theme: 'tertiary',
+};
+
+const buttonWrapper = css`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const sizes = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <div className="description">Small</div>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div>
+        <div className="description">Medium</div>
+        <Button size="medium">BUTTON</Button>
+      </div>
+      <div>
+        <div className="description">Big</div>
+        <Button size="big">BUTTON</Button>
+      </div>
+    </div>
+  );
 };
--- 2022/react/storybook-design-system/src/Button/Button.tsx
+++ 2022/react/storybook-design-system/src/Button/Button.tsx
@@ -3,12 +3,19 @@ import { css } from '@emotion/react';
 
 interface Props {
   children?: React.ReactNode;
+  theme?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'small' | 'medium' | 'big';
   onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ children, onClick }: Props) => {
+const Button = ({
+  children,
+  theme = 'primary',
+  size = 'medium',
+  onClick,
+}: Props) => {
   return (
-    <button css={style} onClick={onClick}>
+    <button css={[style, themes[theme], sizes[size]]} onClick={onClick}>
       {children}
     </button>
   );
@@ -38,3 +45,54 @@ const style = css`
     background: #12b886;
   }
 `;
+
+const themes = {
+  primary: css`
+    background: #20c997;
+    color: white;
+    &:hover {
+      background: #38d9a9;
+    }
+    &:active {
+      background: #12b886;
+    }
+  `,
+  secondary: css`
+    background: #e9ecef;
+    color: #343a40;
+    &:hover {
+      background: #f1f3f5;
+    }
+    &:active {
+      background: #dee2e6;
+    }
+  `,
+  tertiary: css`
+    background: none;
+    color: #20c997;
+    &:hover {
+      background: #e6fcf5;
+    }
+    &:active {
+      background: #c3fae8;
+    }
+  `,
+};
+
+const sizes = {
+  small: css`
+    height: 1.75rem;
+    font-size: 0.75rem;
+    padding: 0 0.875rem;
+  `,
+  medium: css`
+    height: 2.5rem;
+    font-size: 1rem;
+    padding: 0 1rem;
+  `,
+  big: css`
+    height: 3rem;
+    font-size: 1.125rem;
+    padding: 0 1.5rem;
+  `,
+};
