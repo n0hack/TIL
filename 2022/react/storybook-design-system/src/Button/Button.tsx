ba5d6c14e1d06a1cc9c75a91ff540776082652af
n0hack <nohack-@naver.com>
2022-10-15 22:34:46 +0900

    emotion 설치

---
+++ 2022/react/storybook-design-system/src/Button/Button.tsx
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import React from 'react';
+import { css, jsx } from '@emotion/react';
+
+interface Props {
+  children?: React.ReactNode;
+  onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = ({ children, onClick }: Props) => {
+  return (
+    <button css={style} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
+
+const style = css`
+  outline: none;
+  border: none;
+  box-sizing: border-box;
+  height: 2rem;
+  font-size: 0.875rem;
+  padding: 0.5rem 1rem;
+  background: #20c997;
+  color: white;
+  border-radius: 0.25rem;
+  line-height: 1;
+  font-weight: 600;
+  &:focus {
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+  }
+  &:hover {
+    background: #38d9a9;
+  }
+  &:active {
+    background: #12b886;
+  }
+`;
