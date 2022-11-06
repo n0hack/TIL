e6f9d6c9392a4a47e31558eac6b5e79958886553
n0hack <nohack-@naver.com>
2022-11-06 22:44:03 +0900

    05. 데코레이터와 심화 타입

--- books/language/단숨에 배우는 타입스크립트/src/main.ts
+++ books/language/단숨에 배우는 타입스크립트/src/main.ts
@@ -1,32 +1,144 @@
-// enum은 컴파일 시 자바스크립트 객체를 생성하지만, const enum은 아무것도 생성하지 않음
-const enum Direction {
-  UP = 'UP',
-  DOWN = 'DOWN',
-  LEFT = 'LEFT',
-  RIGHT = 'RIGHT',
+function whoAmI(target: Function): void {
+  console.log(`You are:`, target);
 }
 
-// 제네릭은 의존성을 낮춤
-interface Comparator<T> {
-  compareTo(value: T): number;
+@whoAmI
+class Friend {
+  constructor(private name: string, private age: number) {}
 }
 
-class Rectangle implements Comparator<Rectangle> {
-  constructor(private width: number, private height: number) {}
-  compareTo(value: Rectangle): number {
-    return this.width * this.height - value.width * value.height;
+function UIComponent(html: string) {
+  console.log(`The decorator received ${html}\n`);
+  return function (target: Function) {
+    console.log(`Someone wants to create a UI Component from\n${target}`);
+  };
+}
+
+@UIComponent('<h1>Hello Shopper!</h1>')
+class Shopper {
+  constructor(private name: string) {}
+}
+
+// 믹스인 (Mixin) - 특정 동작을 구현하는 클래스
+type constructorMixin = {
+  new (...args: any[]): {};
+};
+
+function useSalutation(salutaion: string) {
+  // 데코레이터 Body
+  return function <T extends constructorMixin>(target: T) {
+    // 클래스 상속 후 재구현
+    return class extends target {
+      private message = 'Hello ' + salutaion + (this as any).name;
+
+      sayHello() {
+        console.log(`${this.message}`);
+      }
+
+      sayGoodbye() {
+        console.log('Bye');
+      }
+    };
+  };
+}
+
+@useSalutation('Mr.')
+class Greeter {
+  constructor(public name: string) {}
+  sayHello() {
+    console.log(`Hello ${this.name}`);
   }
 }
 
-class Triangle implements Comparator<Triangle> {
-  compareTo(value: Triangle): number {
-    return 0;
+const grt = new Greeter('John');
+grt.sayHello();
+(grt as any).sayGoodbye();
+
+/**
+ * 로그 메서드 데코레이터
+ * @param target 메서드를 참조하는 객체
+ * @param key 메서드 이름
+ * @param descriptor 메서드 디스크립터
+ */
+function logTrade(target: any, key: string, descriptor: PropertyDescriptor) {
+  // 원본 저장
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function () {
+    console.log(`Invoked ${key} providing:`, arguments);
+    return originalMethod.apply(this, arguments);
+  };
+
+  return descriptor;
+}
+
+class Trade {
+  @logTrade
+  placeOrder(
+    stockName: string,
+    quantity: number,
+    operation: string,
+    traderID: number
+  ) {
+    console.log('호출');
   }
 }
 
-const rect1 = new Rectangle(2, 5);
-const rect2 = new Rectangle(2, 3);
+const trade = new Trade();
+trade.placeOrder('IBM', 100, 'BUY', 123);
 
-rect1.compareTo(rect2) > 0
-  ? console.log('rect1 is bigger')
-  : console.log('rect2 is bigger');
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+type Modifiable<T> = {
+  -readonly [P in keyof T]: T[P];
+};
+
+type MyPartial<T> = {
+  [P in keyof T]?: T[P];
+};
+
+type MyRequired<T> = {
+  [P in keyof T]-?: T[P];
+};
+
+// 일부만 Pick
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+const roPerson: MyReadonly<Person> = {
+  name: 'NoHack',
+  age: 29,
+};
+
+const pickPerson: MyPick<Person, 'name'> = {
+  name: 'NoHack',
+};
+
+// MyPromise
+type SyncService = {
+  baseUrl: string;
+  getA(): string;
+};
+
+type MyPromise<T> = T extends (...args: infer A) => infer R
+  ? (...args: A) => Promise<R>
+  : T;
+
+type Promisify<T> = {
+  [P in keyof T]: MyPromise<T[P]>;
+};
+
+const test: Promisify<SyncService> = {
+  baseUrl: 's',
+  getA() {
+    return Promise.resolve('');
+  },
+};
