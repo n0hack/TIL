56bc91f18e58e1a2e42ecdf3efa8d9deb08994f1
n0hack <nohack-@naver.com>
2022-11-06 21:43:56 +0900

    04. 열거 타입과 제네릭

--- books/language/단숨에 배우는 타입스크립트/src/main.ts
+++ books/language/단숨에 배우는 타입스크립트/src/main.ts
@@ -1,69 +1,32 @@
-// 싱글톤
-class AppState {
-  counter = 0;
-  private static instanceRef: AppState;
+// enum은 컴파일 시 자바스크립트 객체를 생성하지만, const enum은 아무것도 생성하지 않음
+const enum Direction {
+  UP = 'UP',
+  DOWN = 'DOWN',
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+}
 
-  private constructor() {}
+// 제네릭은 의존성을 낮춤
+interface Comparator<T> {
+  compareTo(value: T): number;
+}
 
-  static getInstance() {
-    if (this.instanceRef === undefined) {
-      this.instanceRef = new AppState();
-    }
-    return this.instanceRef;
+class Rectangle implements Comparator<Rectangle> {
+  constructor(private width: number, private height: number) {}
+  compareTo(value: Rectangle): number {
+    return this.width * this.height - value.width * value.height;
   }
 }
 
-const appState1 = AppState.getInstance();
-const appState2 = AppState.getInstance();
-
-console.log(appState1 === appState2);
-console.log(++appState1.counter);
-console.log(++appState2.counter);
-
-// 인터페이스 프로그래밍
-// 인터페이스와 클래스는 목적부터 다름. (클래스 = 상속받아 확장, 기능 구현을 강제)
-interface Product {
-  id: number;
-  description: string;
-}
-
-interface IProductService {
-  getProducts(): Product[];
-  getProductById(id: number): Product;
-}
-
-class ProductService implements IProductService {
-  getProducts(): Product[] {
-    return [];
-  }
-
-  getProductById(id: number): Product {
-    return { id: 123, description: 'Good product' };
+class Triangle implements Comparator<Triangle> {
+  compareTo(value: Triangle): number {
+    return 0;
   }
 }
 
-class MockProductService implements IProductService {
-  getProducts(): Product[] {
-    return [];
-  }
+const rect1 = new Rectangle(2, 5);
+const rect2 = new Rectangle(2, 3);
 
-  getProductById(id: number): Product {
-    return { id: 456, description: 'Not a real product' };
-  }
-}
-
-const productService = new ProductService();
-const products = productService.getProducts();
-
-// 팩토리 함수(비즈니스 로직을 구현하고, 인스턴스를 반환하는 함수)를 만들어 두면 편함
-function getProductService(isProd: boolean) {
-  if (isProd) {
-    return new ProductService();
-  } else {
-    return new MockProductService();
-  }
-}
-
-const isProd = true;
-const productService2 = getProductService(isProd);
-const products2 = productService2.getProducts();
+rect1.compareTo(rect2) > 0
+  ? console.log('rect1 is bigger')
+  : console.log('rect2 is bigger');
