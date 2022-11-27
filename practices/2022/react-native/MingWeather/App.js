a9ad4115afea78d45e14c537431dd10efd38e35c
Ming <nohack-@naver.com>
2022-11-28 00:27:02 +0900

    Creating The App

---
+++ practices/2022/react-native/MingWeather/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
