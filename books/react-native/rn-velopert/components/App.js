31ea483e78e278e277914401cbb84963238a8e31
Ming <nohack-@naver.com>
2022-11-27 23:08:17 +0900

    02. 컴포넌트 (카운터 만들기)

--- books/react-native/rn-velopert/components/App.js
+++ books/react-native/rn-velopert/components/App.js
@@ -12,21 +12,26 @@ import {
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Box from './components/Box';
+import Counter from './components/Counter';
 import Greeting from './components/Greeting';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
+  const [count, setCount] = useState(0);
 
-  const onPress = () => {
-    setVisible(!visible);
-  };
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
 
   return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-      {visible && <Box rounded size="large" color="blue" />}
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
