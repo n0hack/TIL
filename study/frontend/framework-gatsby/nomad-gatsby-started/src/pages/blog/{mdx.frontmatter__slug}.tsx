09c6d1867eb36656632f99223b092a6fac13d0f2
Ming <nohack-@naver.com>
2022-12-10 19:37:09 +0900

    feat: GraphQL로 MDX 포스트 받아오기

--- study/frontend/framework-gatsby/nomad-gatsby-started/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ study/frontend/framework-gatsby/nomad-gatsby-started/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,29 @@
+import { graphql, PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
+import Seo from '../../components/Seo';
 
-export default function BlogPost() {
-  return (
-    <Layout title="Blog Post">
-      <div />
-    </Layout>
-  );
+export default function BlogPost({
+  data,
+  children,
+}: PageProps<Queries.PostDetailQuery>) {
+  return <Layout title="Blog Post">{children}</Layout>;
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: PageProps<Queries.PostDetailQuery>) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
