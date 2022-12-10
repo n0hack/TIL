0f037986587c66708c0c2bd7cb7950496b400db7
Ming <nohack-@naver.com>
2022-12-10 19:16:20 +0900

    feat: 동적 페이지 (MDX Slug)

--- study/frontend/framework-gatsby/nomad-gatsby-started/src/pages/blog/index.tsx
+++ study/frontend/framework-gatsby/nomad-gatsby-started/src/pages/blog/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { graphql, HeadFC, PageProps } from 'gatsby';
-import Layout from '../components/Layout';
-import Seo from '../components/Seo';
+import { graphql, HeadFC, Link, PageProps } from 'gatsby';
+import Layout from '../../components/Layout';
+import Seo from '../../components/Seo';
 
 export default function Blog({ data }: PageProps<Queries.BlogPostQuery>) {
   return (
@@ -9,13 +9,15 @@ export default function Blog({ data }: PageProps<Queries.BlogPostQuery>) {
       <section>
         {data.allMdx.nodes.map((file, index) => (
           <article key={index}>
-            <h3>{file.frontmatter?.title}</h3>
-            <h5>
-              {file.frontmatter?.author} in {file.frontmatter?.category}
-            </h5>
-            <h6>{file.frontmatter?.date}</h6>
-            <hr />
-            <p>{file.excerpt}</p>
+            <Link to={`/blog/${file.frontmatter?.slug}`}>
+              <h3>{file.frontmatter?.title}</h3>
+              <h5>
+                {file.frontmatter?.author} in {file.frontmatter?.category}
+              </h5>
+              <h6>{file.frontmatter?.date}</h6>
+              <hr />
+              <p>{file.excerpt}</p>
+            </Link>
           </article>
         ))}
       </section>
@@ -32,6 +34,7 @@ export const query = graphql`
           title
           date(formatString: "YYYY.MM.DD")
           author
+          slug
         }
         body
         excerpt(pruneLength: 50)
---
+++ study/frontend/framework-gatsby/nomad-gatsby-started/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+export default function BlogPost() {
+  return (
+    <Layout title="Blog Post">
+      <div />
+    </Layout>
+  );
+}
