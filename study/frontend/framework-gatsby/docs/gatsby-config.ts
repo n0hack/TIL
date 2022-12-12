e5dfaecfe44c317f415e684cefe8ac6c8c80a5d3
Ming <nohack-@naver.com>
2022-12-12 22:58:03 +0900

    Gatsby 플러그인 (StaticImage)

--- study/frontend/framework-gatsby/docs/gatsby-config.ts
+++ study/frontend/framework-gatsby/docs/gatsby-config.ts
@@ -9,7 +9,10 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
 
 export default config
--- study/frontend/framework-gatsby/docs/src/pages/index.tsx
+++ study/frontend/framework-gatsby/docs/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import Layout from '../components/Layout';
 
@@ -5,6 +6,11 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Static"
+      />
+      <StaticImage src="../images/img.jpeg" alt="영복" />
     </Layout>
   );
 };
