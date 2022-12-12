1bf18777813cfafbacf8b1dc1b134d31306b4864
Ming <nohack-@naver.com>
2022-12-12 23:15:50 +0900

    useStaticQuery와 Page Query 차이 공부

--- study/frontend/framework-gatsby/docs/gatsby-config.ts
+++ study/frontend/framework-gatsby/docs/gatsby-config.ts
@@ -9,7 +9,17 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
 
 export default config;
--- study/frontend/framework-gatsby/docs/src/pages/blog.tsx
+++ study/frontend/framework-gatsby/docs/src/pages/blog.tsx
@@ -1,15 +1,32 @@
+import { graphql, PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
-const BlogPage = () => {
+// Gatsby는 원래 페이지에서만 쿼리를 받아올 수 있었지만,
+// useStaticQuery Hook의 등장으로 컴포넌트에서도 쿼리를 받아올 수 있게 되었다.
+const BlogPage = ({ data }: PageProps<Queries.getFilesQuery>) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
 export const Head = () => <Seo title="My Blog Posts" />;
+
+export const query = graphql`
+  query getFiles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export default BlogPage;
