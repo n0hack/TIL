b0cf38ecbf3cc0a29d96d40294245e2ec7aec70e
Ming <nohack-@naver.com>
2022-12-13 22:22:07 +0900

    환경변수 주입

--- study/frontend/framework-gatsby/docs/src/pages/index.tsx
+++ study/frontend/framework-gatsby/docs/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { StaticImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
 const IndexPage = () => {
+  useEffect(() => {
+    (async () => {
+      console.log(process.env.GATSBY_API_URL);
+      const result = await fetch(`${process.env.GATSBY_API_URL}/users`);
+      console.log(result);
+    })();
+  }, []);
+
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
