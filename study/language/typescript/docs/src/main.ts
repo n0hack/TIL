165b26fc53f1df2ec4575d8eb1c82a199e0865ff
Ming <nohack-@naver.com>
2022-12-19 16:48:08 +0900

    Generics

--- study/language/typescript/docs/src/main.ts
+++ study/language/typescript/docs/src/main.ts
@@ -1,1 +1,27 @@
-console.log('hi');
+namespace Generics {
+  class BeeKeeper {
+    hasMask = true;
+  }
+
+  class ZooKeeper {
+    nametag = 'Mikle';
+  }
+
+  class Animal {
+    numLegs = 4;
+  }
+
+  class Bee extends Animal {
+    keeper = new BeeKeeper();
+  }
+
+  class Lion extends Animal {
+    keeper = new ZooKeeper();
+  }
+
+  function createInstance<A extends Animal>(c: new () => A): A {
+    return new c();
+  }
+  // console.log(createInstance(Lion).keeper.nametag);
+  // console.log(createInstance(Bee).keeper.hasMask);
+}
