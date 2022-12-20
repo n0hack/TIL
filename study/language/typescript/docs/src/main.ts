07da569f7e47ab0069b9b2df983b93e57fa638d6
Ming <nohack-@naver.com>
2022-12-21 01:30:13 +0900

    Mapped Types

--- study/language/typescript/docs/src/main.ts
+++ study/language/typescript/docs/src/main.ts
@@ -52,3 +52,55 @@ namespace ConditionalTypes {
     : never;
   type Num = GetReturnType<() => number>;
 }
+
+namespace MappedTypes {
+  type OptionsFlags<T> = {
+    [P in keyof T]: boolean;
+  };
+
+  type FeatureFlags = {
+    darkMode: () => void;
+    newUserProfile: () => void;
+  };
+
+  type FeatureOptions = OptionsFlags<FeatureFlags>;
+
+  // Mutable
+  type CreateMutable<T> = {
+    -readonly [P in keyof T]: T[P];
+  };
+  type LockedAccount = {
+    readonly id: string;
+    readonly name: string;
+  };
+  type UnlockedAccount = CreateMutable<LockedAccount>;
+
+  // Concrete
+  type Concrete<T> = {
+    [P in keyof T]-?: T[P];
+  };
+  type MaybeUser = {
+    id: string;
+    name?: string;
+    age?: number;
+  };
+  type User = Concrete<MaybeUser>;
+
+  // Key Remapping
+  type Getters<T> = {
+    [P in keyof T as `get${Capitalize<string & P>}`]: () => T[P];
+  };
+  interface Person {
+    name: string;
+    age: number;
+    location: string;
+  }
+  type LazyPerson = Getters<Person>;
+
+  type EventConfig<Events extends { kind: string }> = {
+    [E in Events as E['kind']]: (event: E) => void;
+  };
+  type SquareEvent = { kind: 'square'; x: number; y: number };
+  type CircleEvent = { kind: 'circle'; radius: number };
+  type Config = EventConfig<SquareEvent | CircleEvent>;
+}
