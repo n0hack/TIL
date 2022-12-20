6bdf8d1d1e7ff08845dbff4d946c85af9f76c7f8
Ming <nohack-@naver.com>
2022-12-20 09:31:12 +0900

    Conditional Types

--- study/language/typescript/docs/src/main.ts
+++ study/language/typescript/docs/src/main.ts
@@ -36,3 +36,19 @@ namespace Keyof {
   type Mapish = { [k: string]: boolean };
   type M = keyof Mapish;
 }
+
+namespace ConditionalTypes {
+  type MessageOf<T extends { message: unknown }> = T['message'];
+  interface Email {
+    message: string;
+  }
+  type EmailMessageContents = MessageOf<Email>;
+
+  type Flatten<Type> = Type extends Array<infer Item> ? Item : Type;
+  type Str = Flatten<string[]>;
+
+  type GetReturnType<Type> = Type extends (...args: never[]) => infer Return
+    ? Return
+    : never;
+  type Num = GetReturnType<() => number>;
+}
