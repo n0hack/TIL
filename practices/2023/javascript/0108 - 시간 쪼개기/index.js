fc34b9e8111d19511244491444ac8a70fbae656e
Ming <nohack-@naver.com>
2023-01-09 03:34:15 +0900

    시간 쪼개기 완성

--- practices/2023/javascript/0108 - 시간 쪼개기/index.js
+++ practices/2023/javascript/0108 - 시간 쪼개기/index.js
@@ -17,15 +17,15 @@ const schedules = [
     arrivalPortId: 8,
     departurePortId: 1,
   },
-  // {
-  //   landingAt: '2023-01-08T18:44:00',
-  //   departureAt: '2023-01-08T18:59:00',
-  //   arrivalAt: '2023-01-08T20:31:00',
-  //   takeOffAt: '2023-01-08T20:46:00',
-  //   aircraftItemId: 1,
-  //   arrivalPortId: 8,
-  //   departurePortId: 1,
-  // },
+  {
+    landingAt: "2023-01-08T18:44:00",
+    departureAt: "2023-01-08T18:59:00",
+    arrivalAt: "2023-01-08T20:31:00",
+    takeOffAt: "2023-01-08T20:46:00",
+    aircraftItemId: 2,
+    arrivalPortId: 8,
+    departurePortId: 1,
+  },
 ];
 
 const b = {
@@ -33,27 +33,39 @@ const b = {
     {
       firstLandingTime: "2023-01-08T07:00:00",
       lastLandingTime: "2023-01-08T11:21:00",
-      lastTakeOffTime: "2022-01-08T12:53:00",
+      lastTakeOffTime: "2023-01-08T12:53:00",
     },
     {
       firstLandingTime: "2023-01-08T13:50:00",
       lastLandingTime: "2023-01-08T18:44:00",
-      lastTakeOffTime: "2022-01-08T20:45:00",
+      lastTakeOffTime: "2023-01-08T20:45:00",
     },
   ],
   2: [
     {
-      firstLandingTime: "2023-01-08T06:00:00",
+      firstLandingTime: "2023-01-08T05:00:00",
       lastLandingTime: "2023-01-08T08:37:00",
-      lastTakeOffTime: "2022-01-08T08:52:00",
+      lastTakeOffTime: "2023-01-08T10:29:00",
     },
     {
       firstLandingTime: "2023-01-08T14:50:00",
       lastLandingTime: "2023-01-08T19:44:00",
-      lastTakeOffTime: "2022-01-08T21:16:00",
+      lastTakeOffTime: "2023-01-08T21:16:00",
     },
   ],
 };
+
+const flat = (arr) => {
+  return arr.reduce((acc, cur) => {
+    if (Array.isArray(cur)) {
+      return [...acc, ...flat(cur)];
+    } else {
+      return [...acc, cur];
+    }
+  }, []);
+};
+
+// 기본적인 비행시간도 받아야 할 듯.
 
 const originDate = dayjs("2023-01-08");
 // 스케줄 로테이션
@@ -72,30 +84,54 @@ for (const schedule of schedules) {
     lastLandingTime = dayjs(lastLandingTime);
     lastTakeOffTime = dayjs(lastTakeOffTime);
 
-    // 총 비행시간 계산
-    // console.log("gap: ", Math.abs(landingAt.diff(takeOffAt, "minutes")));
+    // 총 비행시간 계산 (이건 실제로 기본 시간 받아서 미리 가공해야 할 듯)
+    const gap = Math.abs(landingAt.diff(takeOffAt, "minutes"));
 
     // 범위에 들어있는지 체크
     if (firstLandingTime.isSameOrBefore(landingAt, "minutes") && lastLandingTime.isSameOrAfter(landingAt, "minutes")) {
-      console.log("범위에 들어옴");
+      const result = [];
+
+      console.log("범위에 들어있는 요청 가능 시간:", requestableTime);
+      console.log(gap);
+
+      // before 가능한지
+      const beforeLastLandingTime = landingAt.subtract(gap + 1, "m");
+
+      // console.log("before");
+      // console.log(firstLandingTime.format("YYYY-MM-DDTHH:mm:00"));
+      // console.log(beforeLastLandingTime.format("YYYY-MM-DDTHH:mm:00"));
+      // console.log(firstLandingTime.isSameOrBefore(beforeLastLandingTime, "minutes"));
+
+      if (firstLandingTime.isSameOrBefore(beforeLastLandingTime, "minutes")) {
+        result.push({
+          firstLandingTime: firstLandingTime.format("YYYY-MM-DDTHH:mm:00"),
+          lastLandingTime: beforeLastLandingTime.format("YYYY-MM-DDTHH:mm:00"),
+          lastTakeOffTime: beforeLastLandingTime.add(gap, "m").format("YYYY-MM-DDTHH:mm:00"),
+        });
+      }
+
+      // after 가능한지
+      let afterTakeOffTime = takeOffAt.add(gap + 1, "m");
+
+      // console.log("after");
+      // console.log(afterTakeOffTime.format("YYYY-MM-DDTHH:mm:00"));
+      // console.log(lastTakeOffTime.format("YYYY-MM-DDTHH:mm:00"));
+      // console.log(afterTakeOffTime.isSameOrBefore(lastTakeOffTime, "minutes"));
+
+      if (afterTakeOffTime.isSameOrBefore(lastTakeOffTime, "minutes")) {
+        result.push({
+          firstLandingTime: takeOffAt.add(1, "m").format("YYYY-MM-DDTHH:mm:00"),
+          lastLandingTime: lastLandingTime.format("YYYY-MM-DDTHH:mm:00"),
+          lastTakeOffTime: lastTakeOffTime.format("YYYY-MM-DDTHH:mm:00"),
+        });
+      }
+
+      return result;
+      // 앞뒤로 쪼개기 수행
+    } else {
+      return requestableTime;
     }
-
-    // console.log(firstLandingTime)
-
-    // console.log("요청 가능 시간:", requestableTime);
   });
 
-  // console.log(dayjs(landingAt).date());
-  // console.log(originDate.isSame(dayjs(landingAt), 'dates'));
+  console.log("최종 결과: ", flat(requestableTimes));
 }
-// const requestableTimes = b[aircraftItemId].map((requestableTime) => {
-
-// });
-
-// console.log(requestableTimes);
-
-const newRequestableTime = [];
-
-// console.log(a);
-// console.log(b);
-// console.log(newRequestableTime);
