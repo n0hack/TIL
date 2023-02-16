c9f0e738567cf9d23c0a6c6284eb942f3282d942
Ming <nohack-@naver.com>
2023-02-16 21:42:31 +0900

    MongoDB 클러스터 생성

--- study/test-driven-development/[23.02.16] 따라하며 배우는 TDD 개발 (John Ahn)/server.js
+++ study/test-driven-development/[23.02.16] 따라하며 배우는 TDD 개발 (John Ahn)/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const productRoutes = require('./routes/products');
 
 const PORT = 52273;
 
 const app = express();
+
+mongoose
+  .connect('mongodb+srv://nohack:121234@cluster0.tmgmtym.mongodb.net/cluster0?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
