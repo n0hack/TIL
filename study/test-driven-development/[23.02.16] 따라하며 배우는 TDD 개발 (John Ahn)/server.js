f16a61ced3badcc14168d6a3db2b3ca27cde8d76
Ming <nohack-@naver.com>
2023-02-16 20:55:54 +0900

    server.js 파일 작성하기

---
+++ study/test-driven-development/[23.02.16] 따라하며 배우는 TDD 개발 (John Ahn)/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = 5000;
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
