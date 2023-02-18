7885f524cf0ab18a307263a828a673d593cd93f9
Ming <nohack-@naver.com>
2023-02-18 20:24:18 +0900

    beforeEach

--- study/test-driven-development/[23.02.16] 따라하며 배우는 TDD 개발 (John Ahn)/test/unit/products.test.js
+++ study/test-driven-development/[23.02.16] 따라하며 배우는 TDD 개발 (John Ahn)/test/unit/products.test.js
@@ -5,18 +5,25 @@ const newProduct = require('../data/new-product.json');
 
 productModel.create = jest.fn();
 
+let req, res, next;
+
+// beforeEach는 각각의 테스트가 실행되기 전에 실행됨
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe('Product Controller Create', () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+
   test('should have a createProduct function', () => {
     expect(typeof productController.createProduct).toBe('function');
   });
 
   test('should call productModel.create', () => {
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
-    let next = null;
-
-    req.body = newProduct;
-
     // 단위 테스트이기 때문에 실제 모델에 영향을 주어서는 안 됨 (Mock을 통해 Spy 심기 - 추적 가능)
     productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
