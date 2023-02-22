4b3c85d8f0eebb3fe01fd8542ea2f20fd6ab99e1
Ming <nohack-@naver.com>
2023-02-22 19:24:12 +0900

    에러 처리를 위한 통합 테스트 작성하기

--- study/tdd/[인프런] 따라하며 배우는 TDD 개발 (John Ahn)/__tests__/integration/products.int.test.js
+++ study/tdd/[인프런] 따라하며 배우는 TDD 개발 (John Ahn)/__tests__/integration/products.int.test.js
@@ -2,9 +2,17 @@ const request = require('supertest');
 const app = require('../../server');
 const newProduct = require('../data/new-product.json');
 
+// 통합 테스트는 실제 DB까지 테스트
 test('POST /api/products', async () => {
   const response = await request(app).post('/api/products').send(newProduct);
   expect(response.statusCode).toBe(201);
   expect(response.body.name).toBe(newProduct.name);
   expect(response.body.description).toBe(newProduct.description);
+});
+
+test('should return 500 on POST /api/products', async () => {
+  const response = await request(app).post('/api/products').send({ name: 'phone' });
+  expect(response.statusCode).toBe(500);
+  console.log('response.body', response.body);
+  expect(response.body).toStrictEqaul({ message: '' });
 });
