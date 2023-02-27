b679ea3ac9c65b35074a6dea6b5f823ddc8b27d8
Ming <nohack-@naver.com>
2023-02-27 23:19:16 +0900

    twin.macro 셋팅

--- practices/2023/frontend/react/02.23 - twin.macro 연습/src/App.tsx
+++ practices/2023/frontend/react/02.23 - twin.macro 연습/src/App.tsx
@@ -3,7 +3,7 @@ import tw from 'twin.macro';
 import './App.css';
 
 function App() {
-  return <div css={tw`text-red-200`}>Hello World</div>;
+  return <div css={tw`mx-auto text-center text-custom`}>Hello World</div>;
 }
 
 export default App;
