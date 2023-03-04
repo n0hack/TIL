eceb0312da380d1ef3459da0a12272727e009f92
Ming <nohack-@naver.com>
2023-03-05 01:37:35 +0900

    Type definition

--- study/frontend/library-react-query/docs/src/App.tsx
+++ study/frontend/library-react-query/docs/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(window.color);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
