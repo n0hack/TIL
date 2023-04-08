6be5d22eee3fd274eff4b8996aa218659b46923a
Ming <nohack-@naver.com>
2023-04-08 17:11:53 +0900

    [노마드코더] GraphQL로 영화 API 만들기 - Resolver

--- study/frontend/graphql/nomad-coders/tweetql-server/src/index.ts
+++ study/frontend/graphql/nomad-coders/tweetql-server/src/index.ts
@@ -5,63 +5,49 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // Scalar Types: String, Boolean, Int, Float, ID와 같이 내장된 타입들
 const typeDefs = `#graphql
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
   }
 
   type Tweet { 
-    id: ID
-    text: String
+    id: ID!
+    text: String!
     author: User
-  }
-
-  type Book {
-    title: String
-    author: String
   }
 
   # REST에서 GET에 대한 것을 정의
   type Query {
-    allTweets: [Tweet]
-    books: [Book]
-    tweets: [Tweet]
-    # Arguments
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    # Arguments (!가 붙으면 non-nullable)
+    tweet(id: ID!): Tweet
   }
 
   # 유저가 보낸 데이터로 mutate하는 모든 것을 정의 (POST, PUT, DELETE 등)
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
 // 데이터셋
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
 const tweets = [
   {
-    text: 'Hello World',
+    id: '1',
+    text: 'First One!',
   },
   {
-    text: 'Hello World2',
+    id: '2',
+    text: 'Second One!',
   },
 ];
 
-// 리졸버
+// 리졸버 (스키마와 형태가 동일해야 함)
 const resolvers = {
   Query: {
-    books: () => books,
-    tweets: () => tweets,
+    allTweets: () => tweets,
+    tweet: (root: any, args: { id: string }) => tweets.find((tweet) => tweet.id === args.id),
   },
 };
 
