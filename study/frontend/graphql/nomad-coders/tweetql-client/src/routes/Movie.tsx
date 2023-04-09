b086bbe4cb606d0cbb87e6e21312794b68159a9b
Ming <nohack-@naver.com>
2023-04-09 15:54:48 +0900

    [노마드코더] GraphQL로 영화 웹 앱 만들기 - 프로젝트 Setup

---
+++ study/frontend/graphql/nomad-coders/tweetql-client/src/routes/Movie.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Movie = () => {
+  return <div>Movie</div>;
+};
+
+export default Movie;
---
+++ study/frontend/graphql/nomad-coders/tweetql-client/src/routes/Movies.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Movies = () => {
+  return <div>This is a list of movies</div>;
+};
+
+export default Movies;
