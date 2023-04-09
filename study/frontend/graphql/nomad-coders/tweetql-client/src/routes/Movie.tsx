5c9ce649b17f615b53bced815c2a1480f669f8cc
Ming <nohack-@naver.com>
2023-04-09 18:11:39 +0900

    [노마드코더] GraphQL로 영화 웹 앱 만들기 - Local State

--- study/frontend/graphql/nomad-coders/tweetql-client/src/routes/Movie.tsx
+++ study/frontend/graphql/nomad-coders/tweetql-client/src/routes/Movie.tsx
@@ -1,20 +1,67 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query ($movieId: String!) {
     movie(id: $movieId) {
       id
       title
-      background_image
+      medium_cover_image
+      rating
+      isLiked @client
     }
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div<{ bg: string }>`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
 const Movie = () => {
   const params = useParams<{ id: string }>();
-  const { data, loading, error } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    error,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: params.id,
     },
@@ -24,11 +71,34 @@ const Movie = () => {
     return <div>Loading...</div>;
   }
 
+  const onClick = () => {
+    // 로컬 캐시를 변경하는 것
+    cache.writeFragment({
+      id: `Movie:${params.id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          title
+          rating
+          isLiked
+        }
+      `,
+      data: {
+        title: 'Hello',
+        rating: 10,
+        isLiked: !data.movie.isLiked,
+      },
+    });
+  };
+
   return (
-    <div>
-      <h1>{data.movie.title}</h1>
-      <img src={data.movie.background_image} style={{ objectFit: 'cover', width: 500 }} />
-    </div>
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading...' : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button onClick={onClick}>{data.movie.isLiked ? 'Unlike' : 'Like'}</button>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
   );
 };
 
--- study/frontend/graphql/nomad-coders/tweetql-client/src/routes/Movies.tsx
+++ study/frontend/graphql/nomad-coders/tweetql-client/src/routes/Movies.tsx
@@ -1,12 +1,14 @@
 import { gql, useApolloClient, useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const ALL_MOVIES = gql`
   query getMovies {
     allMovies {
       title
       id
+      medium_cover_image
     }
     allTweets {
       id
@@ -16,6 +18,63 @@ const ALL_MOVIES = gql`
       }
     }
   }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+const PosterContainer = styled.div`
+  height: 400px;
+  border-radius: 7px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const PosterBg = styled.div<{ background: string }>`
+  background-image: url(${(props) => props.background});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
 `;
 
 const Movies = () => {
@@ -32,20 +91,21 @@ const Movies = () => {
   console.log(data);
 
   return (
-    <ul>
-      <h1>Movies</h1>
-      {data.allMovies.map((movie: any) => (
-        <li key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
-      ))}
-      <h1>Tweets</h1>
-      {data.allTweets.map((tweet: any) => (
-        <li key={tweet.id}>
-          {tweet.text} / by: {tweet.author.fullName}
-        </li>
-      ))}
-    </ul>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <MoviesGrid>
+        {data?.allMovies?.map((movie: any) => (
+          <PosterContainer key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <PosterBg background={movie.medium_cover_image} />
+            </Link>
+          </PosterContainer>
+        ))}
+      </MoviesGrid>
+    </Container>
   );
 };
 
