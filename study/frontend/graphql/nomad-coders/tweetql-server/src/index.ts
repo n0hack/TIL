ad1dc09011f723d728c05f1974a8088f85e2fbb9
Ming <nohack-@naver.com>
2023-04-09 14:54:42 +0900

    [노마드코더] GraphQL로 영화 API 만들기 - REST to GraphQL 마이그레이션

--- study/frontend/graphql/nomad-coders/tweetql-server/src/index.ts
+++ study/frontend/graphql/nomad-coders/tweetql-server/src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import axios from 'axios';
 
 // 스키마
 // Scalar Types: String, Boolean, Int, Float, ID와 같이 내장된 타입들
@@ -25,10 +26,12 @@ const typeDefs = `#graphql
 
   # REST에서 GET에 대한 것을 정의
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     # Arguments (!가 붙으면 non-nullable)
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
 
   # 유저가 보낸 데이터로 mutate하는 모든 것을 정의 (POST, PUT, DELETE 등)
@@ -36,6 +39,30 @@ const typeDefs = `#graphql
     postTweet(text: String!, userId: ID!): Tweet!
     """트윗 삭제"""
     deleteTweet(id: ID!): Boolean!
+  }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String!]!
+    summary: String!
+    description_full: String!
+    synopsis: String!
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -69,12 +96,22 @@ const tweets = [
 // 리졸버 (스키마와 형태가 동일해야 함)
 const resolvers = {
   Query: {
+    allMovies: async () => {
+      const { data } = await axios.get('https://yts.mx/api/v2/list_movies.json/yts-proxy.now.sh/list_movies.json');
+      return data.data.movies;
+    },
     allUsers: () => {
       console.log('All Users Resolver Called!');
       return users;
     },
     allTweets: () => tweets,
     tweet: (root: any, args: { id: string }) => tweets.find((tweet) => tweet.id === args.id),
+    movie: async (_: any, args: { id: string }) => {
+      const { data } = await axios.get(
+        `https://yts.mx/api/v2/movie_details.json/yts-proxy.now.sh/movie_details.json?movie_id=${args.id}`
+      );
+      return data.data.movie;
+    },
   },
   Mutation: {
     postTweet(_: any, { text, userId }: any) {
