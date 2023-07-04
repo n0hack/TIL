c99a50f2759f2a718a39b5d84428fd4ead7b32d3
Lucid <nohack-@naver.com>
2023-07-04 21:10:15 +0900

    Required<Type>

--- practices/language/typescript/[23.07.04] 유틸 타입 직접 만들어보기/index.ts
+++ practices/language/typescript/[23.07.04] 유틸 타입 직접 만들어보기/index.ts
@@ -1,4 +1,4 @@
-// Partial<Type>
+// Partial<Type>: 모든 프로퍼티를 옵셔널하게 만들어주는 유틸리티 타입
 namespace Partial {
   type MyPartial<T> = {
     [P in keyof T]?: T[P];
@@ -24,4 +24,19 @@ namespace Partial {
 
   console.log(todo1);
   console.log(todo2);
+}
+
+// Required<Type>: 모든 프로퍼티를 필수로 만들어주는 유틸리티 타입
+namespace Required {
+  type MyRequired<T> = {
+    [P in keyof T]-?: T[P];
+  };
+
+  interface Props {
+    a?: number;
+    b?: string;
+  }
+
+  const obj: Props = { a: 5 };
+  const obj2: MyRequired<Props> = { a: 5, b: 'hello' };
 }
