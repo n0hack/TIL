8592c504890bd27df01d34c04e1d6cfd4a29b938
Lucid <nohack-@naver.com>
2023-07-10 04:40:55 +0900

    Singleton Pattern

--- study/frontend/react/[문서] Patterns/src/App.jsx
+++ study/frontend/react/[문서] Patterns/src/App.jsx
@@ -4,7 +4,12 @@ import Counter from './patterns/design/Singletone/counter';
 function App() {
   console.log('App:', Counter.getCount());
 
-  return <Singleton />;
+  return (
+    <>
+      <Singleton />
+      <button onClick={() => window.alert(Counter.getCount())}>값 확인</button>
+    </>
+  );
 }
 
 export default App;
