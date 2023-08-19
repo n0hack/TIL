7751d09340ea65c6c34ceca79ccba38afc2e41a3
Lucid <nohack-@naver.com>
2023-08-19 21:02:51 +0900

    MongoDB 클러스터 생성

--- 동영상 강의/test/[인프런] 따라하며 배우는 TDD 개발 (John Ahn)/server.js
+++ 동영상 강의/test/[인프런] 따라하며 배우는 TDD 개발 (John Ahn)/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import productRoutes from './routes.js';
+import mongoose from 'mongoose';
 
-const PORT = 52273;
+mongoose
+  .connect('mongodb+srv://nohack:121234@cluster0.tmgmtym.mongodb.net/cluster0?retryWrites=true&w=majority')
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log(err));
 
 const app = express();
 
@@ -12,6 +16,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+const PORT = 52273;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
