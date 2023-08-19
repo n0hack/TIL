bc74b0d955f104623b109cb974b90b1fa397dfce
Lucid <nohack-@naver.com>
2023-08-19 21:08:52 +0900

    몽구스 Model, Schema 생성

---
+++ 동영상 강의/test/[인프런] 따라하며 배우는 TDD 개발 (John Ahn)/model/Product.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
