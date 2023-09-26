2721b7902d4055050f33523553437bb1800f1024
Lucid <nohack-@naver.com>
2023-09-26 14:29:11 +0900

    [Gatsby] 튜토리얼 - Add Features with Plugins

--- 공식 문서/frontend/gatsby/src/pages/index.tsx
+++ 공식 문서/frontend/gatsby/src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { HeadFC, Link } from 'gatsby';
 import React from 'react';
 import { Layout } from '../components/Layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/icon.png"
+      />
     </Layout>
   );
 };
