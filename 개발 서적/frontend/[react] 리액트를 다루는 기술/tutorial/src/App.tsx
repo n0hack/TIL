8ebe6cb0c530c50e815da9ac046427e41caf7aab
Lucid <nohack-@naver.com>
2023-09-28 08:10:53 +0900

    09. 컴포넌트 스타일링

--- 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
+++ 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { useInputs } from './hooks/useInputs';
+import { SassComponent } from './components/SassComponent';
 
 const App = () => {
   const [state, onChange] = useInputs({ name: '', nickname: '' });
 
   return (
     <div>
-      <div>
-        <input name="name" value={state.name} onChange={onChange('name')} />
-        <input name="nickname" value={state.nickname} onChange={onChange('nickname')} />
-      </div>
-      <div>
-        <p>이름: {state.name}</p>
-        <p>닉네임: {state.nickname}</p>
-      </div>
+      <SassComponent />
     </div>
   );
 };
