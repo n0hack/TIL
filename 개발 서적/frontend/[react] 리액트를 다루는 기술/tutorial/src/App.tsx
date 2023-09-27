85be5afc71e7f28116caa744e326077a6eea1425
Lucid <nohack-@naver.com>
2023-09-28 06:37:49 +0900

    05. ref: DOM에 이름 달기

--- 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
+++ 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  scrollBox: ScrollBox | null;
+
+  constructor(props: any) {
+    super(props);
+    this.scrollBox = null;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox?.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
 }
 
 export default App;
