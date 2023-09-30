4fd63ab4bace8566382eb2633d07989a3425ff6f
Lucid <nohack-@naver.com>
2023-09-30 17:57:58 +0900

    12. immer를 사용하여 더 쉽게 불변성 유지하기

--- 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
+++ 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useInputs } from './hooks/useInputs';
 import { SassComponent } from './components/SassComponent';
+import Immer from './components/Immer';
 
 const App = () => {
   const [state, onChange] = useInputs({ name: '', nickname: '' });
 
   return (
     <div>
-      <SassComponent />
+      <Immer />
     </div>
   );
 };
