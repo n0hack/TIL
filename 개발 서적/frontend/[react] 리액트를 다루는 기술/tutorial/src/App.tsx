32f4c11f7b15c3e6e7a91b25d7b7d09f9fa0f74f
Lucid <nohack-@naver.com>
2023-10-01 11:13:53 +0900

    13. 리액트 라우터로 SPA 개발하기

--- 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
+++ 개발 서적/frontend/[react] 리액트를 다루는 기술/tutorial/src/App.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import { useInputs } from './hooks/useInputs';
 import { SassComponent } from './components/SassComponent';
 import Immer from './components/Immer';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 const App = () => {
   const [state, onChange] = useInputs({ name: '', nickname: '' });
 
   return (
     <div>
-      <Immer />
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/lucid">루시드</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">홍길동</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
     </div>
   );
 };
