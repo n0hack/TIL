909bd4dfb16cd022e96b4a794217b6c706fba08a
Lucid <nohack-@naver.com>
2023-10-06 12:40:56 +0900

    [Astro] 튜토리얼

---
+++ 공식 문서/frontend/astro/src/components/Greeting.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props ={
+  message: string[];
+}
+
+const Greeting = ({ message }: Props) => {
+  return <div>Greeting</div>;
+};
+
+export default Greeting;
--- 공식 문서/frontend/astro/src/pages/posts/post-1.md
+++ 공식 문서/frontend/astro/src/pages/posts/post-1.md
@@ -1,4 +1,5 @@
 ---
+layout: ../../layouts/MarkdownPostLayout.astro
 title: 'My First Blog Post'
 pubDate: 2022-07-01
 description: 'This is the first post of my new Astro blog.'
