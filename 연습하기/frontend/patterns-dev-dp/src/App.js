3c1d433f75738d9ed014c95422f02819a3e7efbb
Lucid <nohack-@naver.com>
2023-10-16 12:09:34 +0900

    Observer 패턴

--- 연습하기/frontend/patterns-dev-dp/src/App.js
+++ 연습하기/frontend/patterns-dev-dp/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ProxyComponent from './patterns/ProxyComponent';
+import ObserverComponent from './patterns/ObserverComponent';
 
 function App() {
   return (
     <div className="App">
-      <ProxyComponent />
+      <ObserverComponent />
     </div>
   );
 }
---
+++ 연습하기/frontend/patterns-dev-dp/src/patterns/observer.js
@@ -0,0 +1,19 @@
+class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(f) {
+    this.observers.push(f);
+  }
+
+  unsubscribe(f) {
+    this.observers = this.observers.filter((subscriber) => subscriber !== f);
+  }
+
+  notify(data) {
+    this.observers.forEach((observer) => observer(data));
+  }
+}
+
+export default Observable;
