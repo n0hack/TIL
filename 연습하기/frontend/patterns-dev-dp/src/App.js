dd0c893cec9c891b4e02585ea5efcd7e5a044287
Lucid <nohack-@naver.com>
2023-10-16 15:48:17 +0900

    Compound 패턴

--- 연습하기/frontend/patterns-dev-dp/src/App.js
+++ 연습하기/frontend/patterns-dev-dp/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import ProxyComponent from './patterns/ProxyComponent';
 import ObserverComponent from './patterns/ObserverComponent';
 import RenderPropsComponent from './patterns/RenderPropsComponent';
+import Flyout from './patterns/CompoundComponent/Flyout';
 
 function App() {
   return (
     <div className="App">
-      <RenderPropsComponent>{({ data }) => <div>hi {data}</div>}</RenderPropsComponent>
+      <Flyout>
+        <Flyout.Toggle />
+        <Flyout.List>
+          <Flyout.Item>Item1</Flyout.Item>
+          <Flyout.Item>Item2</Flyout.Item>
+        </Flyout.List>
+      </Flyout>
     </div>
   );
 }
