c73375957064e89c6761aeaa53d1ee757cc1eeec
Lucid <nohack-@naver.com>
2023-10-17 20:29:54 +0900

    전문가처럼 타입스크립트 infer 사용하기

--- 연습하기/frontend/[2023.10.17] 전문가처럼 타입스크립트 infer 사용하기/infer.ts
+++ 연습하기/frontend/[2023.10.17] 전문가처럼 타입스크립트 infer 사용하기/infer.ts
@@ -3,3 +3,25 @@ type T1 = () => string;
 
 type UnpackedArray<T> = T extends (infer U)[] ? U : T;
 type U0 = UnpackedArray<T0>;
+
+type UnpackedFn<T> = T extends (...args: any[]) => infer U ? U : T;
+type U1 = UnpackedFn<T1>;
+
+type Unpacked<T> = T extends (infer U)[]
+  ? U
+  : T extends (...args: any[]) => infer U
+  ? U
+  : T extends Promise<infer U>
+  ? U
+  : T;
+
+type User = {
+  id: number;
+  name: string;
+};
+
+type PropertyType<T> = T extends { id: infer U; name: infer R } ? [U, R] : T;
+type U3 = PropertyType<User>;
+
+type PropertyType2<T> = T extends { id: infer U; name: infer U } ? U : T;
+type U4 = PropertyType2<User>;
