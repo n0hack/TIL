1f1313f66a2755548360e771174aaf8d5f2e1baa
Lucid <nohack-@naver.com>
2023-10-19 02:36:04 +0900

    불필요한 파일 제거

--- 연습하기/frontend/patterns-dev-dp/src/patterns/RenderPropsComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const RenderPropsComponent = (props) => {
-  console.log(props);
-  return <div>{props.children({ data: 5 })}</div>;
-};
-
-export default RenderPropsComponent;
