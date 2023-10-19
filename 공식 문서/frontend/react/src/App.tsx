97f6cbf93bbc9f5a7cf69e8bc51ab8e694943d91
Lucid <nohack-@naver.com>
2023-10-19 21:24:33 +0900

    SSR

--- 공식 문서/frontend/react/src/App.tsx
+++ 공식 문서/frontend/react/src/App.tsx
@@ -1,7 +1,16 @@
-import UseSyncExternalStore from './hooks/UseSyncExternalStore';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
 
 function App() {
-  return <UseSyncExternalStore />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
