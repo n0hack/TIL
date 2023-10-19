2026a16037f332145f1f48575a5d25b7cd87c83b
Lucid <nohack-@naver.com>
2023-10-19 21:10:00 +0900

    useSyncExternalStore

--- 공식 문서/frontend/react/src/App.tsx
+++ 공식 문서/frontend/react/src/App.tsx
@@ -1,8 +1,3 @@
-// import './App.css';
-import React from 'react';
-import UseDebugValue from './hooks/UseDebugValue';
-import UseDeferredValue from './hooks/UseDeferredValue';
-import UseTransition from './hooks/UseTransition';
 import UseSyncExternalStore from './hooks/UseSyncExternalStore';
 
 function App() {
