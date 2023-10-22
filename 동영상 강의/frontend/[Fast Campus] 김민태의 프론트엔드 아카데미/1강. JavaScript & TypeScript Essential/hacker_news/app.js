c810119eb7c92148aae183f2242122e868fcddeb
Lucid <nohack-@naver.com>
2023-10-22 22:02:36 +0900

    문자열을 활용한 HTML 다루기

--- 동영상 강의/frontend/[Fast Campus] 김민태의 프론트엔드 아카데미/1강. JavaScript & TypeScript Essential/hacker_news/app.js
+++ 동영상 강의/frontend/[Fast Campus] 김민태의 프론트엔드 아카데미/1강. JavaScript & TypeScript Essential/hacker_news/app.js
@@ -1,19 +1,37 @@
-const container = document.getElementById('root');
-
-const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 // Mocking할 때, @id 같은 값을 사용하면, 이후 사용할 때 변경하기 편함
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+async function getData(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
 
-const newsFeed = JSON.parse(ajax.response);
+const container = document.getElementById('root');
 const ul = document.createElement('ul');
+const content = document.createElement('div');
+
+getData(NEWS_URL).then((newsFeed) => {
+  for (let i = 0; i < newsFeed.length; i++) {
+    // DOM 구조가 잘 보이지 않는 것을, DOM API가 아닌, 문자열만 사용해 만듦으로써 보여줌
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `;
+
+    // ul.append(div.children[0]);
+    ul.append(div.firstElementChild);
+  }
+});
 
 // #(해시)가 변경되었을 때, 발생하는 이벤트 (id가 있는 요소로 이동했을 때)
-window.addEventListener('hashchange', (e) => {
+window.addEventListener('hashchange', () => {
   const id = location.hash.slice(1);
 
   fetch(`${CONTENT_URL.replace('@id', id)}`)
@@ -25,17 +43,6 @@ window.addEventListener('hashchange', (e) => {
       content.append(title);
     });
 });
-
-for (let i = 0; i < newsFeed.length; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-  li.append(a);
-  ul.append(li);
-}
 
 container.append(ul);
 container.append(content);
