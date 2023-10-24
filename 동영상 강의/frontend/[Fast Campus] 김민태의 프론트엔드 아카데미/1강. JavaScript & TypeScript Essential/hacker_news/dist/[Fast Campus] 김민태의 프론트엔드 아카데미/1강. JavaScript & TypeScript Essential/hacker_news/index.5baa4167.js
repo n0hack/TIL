69530fce4f8ce6973beac9161e1fcf5df872e5cc
Lucid <nohack-@naver.com>
2023-10-24 21:42:29 +0900

    라우터 만들기

--- 동영상 강의/frontend/[Fast Campus] 김민태의 프론트엔드 아카데미/1강. JavaScript & TypeScript Essential/hacker_news/dist/[Fast Campus] 김민태의 프론트엔드 아카데미/1강. JavaScript & TypeScript Essential/hacker_news/index.5baa4167.js
+++ 동영상 강의/frontend/[Fast Campus] 김민태의 프론트엔드 아카데미/1강. JavaScript & TypeScript Essential/hacker_news/dist/[Fast Campus] 김민태의 프론트엔드 아카데미/1강. JavaScript & TypeScript Essential/hacker_news/index.5baa4167.js
@@ -575,43 +575,52 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"igcvL":[function(require,module,exports) {
-const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 // Mocking할 때, @id 같은 값을 사용하면, 이후 사용할 때 변경하기 편함
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+// Elements
+const container = document.getElementById("root");
+const content = document.createElement("div");
 async function getData(url) {
     const res = await fetch(url);
     const data = await res.json();
     return data;
 }
-const container = document.getElementById("root");
-const ul = document.createElement("ul");
-const content = document.createElement("div");
-getData(NEWS_URL).then((newsFeed)=>{
-    for(let i = 0; i < newsFeed.length; i++){
-        // DOM 구조가 잘 보이지 않는 것을, DOM API가 아닌, 문자열만 사용해 만듦으로써 보여줌
-        const div = document.createElement("div");
-        div.innerHTML = `
+async function newsFeed() {
+    const newsFeed = await getData(NEWS_URL);
+    const newsList = [];
+    newsList.push("<ul>");
+    for (const feed of newsFeed)// DOM 구조가 잘 보이지 않는 것을, DOM API가 아닌, 문자열만 사용해 만듦으로써 보여줌
+    // 의외로 createElement 등으로 관계를 만들어가는 것보다 구조에 대한 가독성이 좋음
+    newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
-          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        <a href="#${feed.id}">
+          ${feed.title} (${feed.comments_count})
         </a>
       </li>
-    `;
-        // ul.append(div.children[0]);
-        ul.append(div.firstElementChild);
-    }
-});
-// #(해시)가 변경되었을 때, 발생하는 이벤트 (id가 있는 요소로 이동했을 때)
-window.addEventListener("hashchange", ()=>{
+    `);
+    newsList.push("</ul>");
+    container.innerHTML = newsList.join("");
+}
+async function newsDetail() {
     const id = location.hash.slice(1);
-    fetch(`${CONTENT_URL.replace("@id", id)}`).then((res)=>res.json()).then((newsContent)=>{
-        const title = document.createElement("h1");
-        title.textContent = newsContent.title;
-        content.append(title);
-    });
-});
-container.append(ul);
-container.append(content);
+    const newsContent = await getData(CONTENT_URL.replace("@id", id));
+    container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">\u{BAA9}\u{B85D}\u{C73C}\u{B85C}</a>
+    </div>
+  `;
+}
+function router() {
+    const routePath = location.hash;
+    if (routePath === "") newsFeed();
+    else newsDetail();
+}
+// #(해시)가 변경되었을 때, 발생하는 이벤트 (id가 있는 요소로 이동했을 때)
+window.addEventListener("hashchange", router);
+router();
 
 },{}]},["gNemz","igcvL"], "igcvL", "parcelRequire84d7")
 
