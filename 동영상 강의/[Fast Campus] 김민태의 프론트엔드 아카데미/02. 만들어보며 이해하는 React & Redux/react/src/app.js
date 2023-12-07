13724a552f25dfca9413c857f95e06872e95b4f9
Lucid <nohack-@naver.com>
2023-12-08 03:15:35 +0900

    07. 클래스 컴포넌트

--- 동영상 강의/[Fast Campus] 김민태의 프론트엔드 아카데미/02. 만들어보며 이해하는 React & Redux/react/src/app.js
+++ 동영상 강의/[Fast Campus] 김민태의 프론트엔드 아카데미/02. 만들어보며 이해하는 React & Redux/react/src/app.js
@@ -1,17 +1,23 @@
 /* @jsx createElement */
 // babel에 의해 jsx 코드는 createElement 함수로 변환된다.
 // 코드상으로는 createElement를 사용하고 있지 않지만, 번들에서는 포함하고 있기에 불러와야 한다.
-import { createElement, render } from './react';
+import { Component, createElement, render } from './react';
 
-function Title(props) {
-  return <h1>{props.children}</h1>;
+class Title extends Component {
+  render() {
+    return <h1>{this.props.children}</h1>;
+  }
 }
+
+// function Title(props) {
+//   return <h1>{props.children}</h1>;
+// }
 
 function Item(props) {
   return <li style={`color: ${props.color}`}>{props.children}</li>;
 }
 
-const vdom = (
+const App = () => (
   <div>
     <Title>React 잘 만들기</Title>
     <ul>
@@ -22,4 +28,4 @@ const vdom = (
   </div>
 );
 
-render(vdom, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
--- 동영상 강의/[Fast Campus] 김민태의 프론트엔드 아카데미/02. 만들어보며 이해하는 React & Redux/react/src/react.js
+++ 동영상 강의/[Fast Campus] 김민태의 프론트엔드 아카데미/02. 만들어보며 이해하는 React & Redux/react/src/react.js
@@ -1,3 +1,9 @@
+export class Component {
+  constructor(props) {
+    this.props = props;
+  }
+}
+
 export function createDOM(node) {
   if (typeof node === 'string') {
     return document.createTextNode(node);
@@ -12,16 +18,25 @@ export function createDOM(node) {
   return element;
 }
 
+function makeProps(props, children) {
+  return { ...props, children: children.length === 1 ? children[0] : children };
+}
+
 export function createElement(tag, props, ...children) {
   // null에 대한 방어 코드
   props = props || {};
 
   // 함수인 경우에 대한 처리
   if (typeof tag === 'function') {
-    if (children.length > 0) {
-      return tag({ ...props, children: children.length === 1 ? children[0] : children });
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
     } else {
-      return tag(props);
+      if (children.length > 0) {
+        return tag(makeProps(props, children));
+      } else {
+        return tag(props);
+      }
     }
   } else {
     return { tag, props, children };
