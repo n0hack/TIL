7ab31de3f05a712493f3a9cba988d70bdf68098a
Lucid <nohack-@naver.com>
2024-04-23 02:34:57 +0900

    불필요한 파일 제거

--- 시리즈/[Youtube] Code With Antonio/restapi/src/router/authentication.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { login, register } from '../controllers/authentication';
-
-export default (router: express.Router) => {
-  router.post('/auth/register', register);
-  router.post('/auth/login', login);
-};
--- 시리즈/개발자 황준일/01. Vanilla JavaScript로 웹 컴포넌트 만들기/src/main.ts
+++
@@ -1,3 +0,0 @@
-import App from './App';
-
-new App(document.getElementById('app') as HTMLElement);
