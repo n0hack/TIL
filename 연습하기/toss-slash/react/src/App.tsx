f5161fceb5b7430f72c92c19d22b424747c91363
Lucid <nohack-@naver.com>
2024-04-25 00:36:14 +0900

    ClickArea 컴포넌트

--- 연습하기/toss-slash/react/src/App.tsx
+++ 연습하기/toss-slash/react/src/App.tsx
@@ -1,12 +1,11 @@
+import { ClickArea } from './components/ClickArea';
 import { OpenGraph } from './components/OpenGraph';
 
 function App() {
   return (
-    <OpenGraph
-      title="공동인증서 없이 바로 등본 떼기 📄"
-      description="가장 가까운 토스 주민센터에서 가능해요."
-      imageUrl="https://static.toss.im/assets/paper0/pc_og.png"
-    />
+    <div>
+      <ClickArea>ㅎㅇ</ClickArea>
+    </div>
   );
 }
 
---
+++ 연습하기/toss-slash/react/src/utils/noop.ts
@@ -0,0 +1,3 @@
+export const noop = () => {};
+
+export const asyncNoop = async () => {};
