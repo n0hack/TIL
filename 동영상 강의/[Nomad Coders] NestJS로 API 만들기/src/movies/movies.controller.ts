47aaa442ac84dfd4dcc75c3dfa8f473f20853da9
Lucid <nohack-@naver.com>
2024-05-08 05:48:41 +0900

    Movies Controller 데코레이터 추가

--- 동영상 강의/[Nomad Coders] NestJS로 API 만들기/src/movies/movies.controller.ts
+++ 동영상 강의/[Nomad Coders] NestJS로 API 만들기/src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +7,33 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    console.log(searchingYear);
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Put('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
