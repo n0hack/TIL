f0a85ec94e4371a7d982588555c3f2cde791f619
Lucid <nohack-@naver.com>
2024-05-08 22:30:50 +0900

    유닛 테스트 추가

--- 동영상 강의/[Nomad Coders] NestJS로 API 만들기/src/movies/movies.controller.ts
+++ 동영상 강의/[Nomad Coders] NestJS로 API 만들기/src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
@@ -11,11 +11,6 @@ export class MoviesController {
   @Get()
   getAll(): Movie[] {
     return this.moviesService.getAll();
-  }
-
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after: ${searchingYear}`;
   }
 
   @Get(':id')
