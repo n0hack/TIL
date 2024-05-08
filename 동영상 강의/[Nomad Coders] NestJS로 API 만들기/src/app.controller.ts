a66f9da26535bb7a6d54e3f3954f290161e6a3d4
Lucid <nohack-@naver.com>
2024-05-09 02:46:20 +0900

    E2E 테스트 추가

--- 동영상 강의/[Nomad Coders] NestJS로 API 만들기/src/app.controller.ts
+++ 동영상 강의/[Nomad Coders] NestJS로 API 만들기/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('app')
+@Controller()
 export class AppController {
   @Get()
   home() {
--- 동영상 강의/[Nomad Coders] NestJS로 API 만들기/test/app.e2e-spec.ts
+++ 동영상 강의/[Nomad Coders] NestJS로 API 만들기/test/app.e2e-spec.ts
@@ -1,24 +1,85 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach에서 변경한 이유는 매 테스트마다 새로운 앱 인스턴스를 만들지 않기 위함
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    test('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    test('POST 200', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: '테스트 영화',
+          genres: ['test'],
+          year: 2000,
+        })
+        .expect(201);
+    });
+
+    test('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: '테스트 영화',
+          genres: ['test'],
+          year: 2000,
+          other: 'thing',
+        })
+        .expect(400);
+    });
+
+    test('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    test('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+
+    test('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+
+    test('UPDATE', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({
+          title: 'Updated Test',
+        })
+        .expect(200);
+    });
+
+    test('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
