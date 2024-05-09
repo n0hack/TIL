004c403600480e0bc1fa31506a6678739674e583
Lucid <nohack-@naver.com>
2024-05-10 05:11:14 +0900

    모듈 분리

--- 개발 서적/[backend] NestJS로 배우는 백엔드 프로그래밍/user-service/src/app.module.ts
+++ 개발 서적/[backend] NestJS로 배우는 백엔드 프로그래밍/user-service/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- 개발 서적/[backend] NestJS로 배우는 백엔드 프로그래밍/user-service/src/users/users.module.ts
+++ 개발 서적/[backend] NestJS로 배우는 백엔드 프로그래밍/user-service/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { EmailService } from 'src/email/email.service';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  providers: [UsersService],
 })
 export class UsersModule {}
