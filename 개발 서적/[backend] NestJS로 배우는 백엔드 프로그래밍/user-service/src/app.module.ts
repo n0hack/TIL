6f863170f2348591e5d9d589d2721cea77fb84af
Lucid <nohack-@naver.com>
2024-05-12 20:54:18 +0900

    TypeORM 설치

--- 개발 서적/[backend] NestJS로 배우는 백엔드 프로그래밍/user-service/src/app.module.ts
+++ 개발 서적/[backend] NestJS로 배우는 백엔드 프로그래밍/user-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -13,6 +14,16 @@ import { validationSchema } from './config/validationSchema';
       validationSchema,
     }),
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'test',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 개발 환경에서만 사용
+    }),
   ],
   controllers: [],
   providers: [],
