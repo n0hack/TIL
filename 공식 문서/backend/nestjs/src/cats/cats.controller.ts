552f308e6f8be71f3b00423776abf94a4263a596
Lucid <nohack-@naver.com>
2024-05-16 07:23:40 +0900

    가드(Guard)

--- 공식 문서/backend/nestjs/src/cats/cats.controller.ts
+++ 공식 문서/backend/nestjs/src/cats/cats.controller.ts
@@ -1,13 +1,25 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { RolesGuard } from 'src/common/guard/roles.guard';
 
+@UseGuards(RolesGuard)
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @Roles(['admin'])
   create(@Body() dto: CreateCatDto) {
     this.catsService.create(dto);
   }
