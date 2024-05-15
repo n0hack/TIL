327c34ffa1f782082d63d2e9ca1d91c7fd157af1
Lucid <nohack-@naver.com>
2024-05-16 03:52:12 +0900

    미들웨어(Middleware)

--- 공식 문서/backend/nestjs/src/app.module.ts
+++ 공식 문서/backend/nestjs/src/app.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { logger } from './common/middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+      )
+      .forRoutes(CatsController);
+  }
+}
