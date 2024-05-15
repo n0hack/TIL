b5e4dd61da0f0ad1b8fe8c87c0ffa006d24b864f
Lucid <nohack-@naver.com>
2024-05-16 08:05:47 +0900

    커스텀 데코레이터(Custom Decorator)

--- 공식 문서/backend/nestjs/src/cats/cats.controller.ts
+++ 공식 문서/backend/nestjs/src/cats/cats.controller.ts
@@ -12,6 +12,7 @@ import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { RolesGuard } from 'src/common/guard/roles.guard';
+import { UniqueCat } from 'src/common/decorators/cat.decorator';
 
 @UseGuards(RolesGuard)
 @Controller('cats')
@@ -29,14 +30,19 @@ export class CatsController {
     return this.catsService.findAll();
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: number): Promise<Cat> {
-    return this.catsService.findOne(id);
+  @Get('unique')
+  async findUniqueCat(@UniqueCat('name') cat) {
+    return cat;
   }
 
   @Get('change-http-code')
   @HttpCode(201)
   changeHttpCode(): string {
     return 'This action should return a status code of 201';
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Cat> {
+    return this.catsService.findOne(id);
   }
 }
