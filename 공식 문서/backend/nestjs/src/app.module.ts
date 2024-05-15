8bdbb4f5c09f35df6423ed85313cf5ae50a4f5b3
Lucid <nohack-@naver.com>
2024-05-16 04:34:16 +0900

    예외 필터(Exception Filter)

--- 공식 문서/backend/nestjs/src/app.module.ts
+++ 공식 문서/backend/nestjs/src/app.module.ts
@@ -7,9 +7,20 @@ import {
 import { CatsModule } from './cats/cats.module';
 import { logger } from './common/middleware/logger.middleware';
 import { CatsController } from './cats/cats.controller';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 @Module({
   imports: [CatsModule],
+  providers: [
+    /**
+     * app.useGlobalFilters(new HttpExceptionFilter());
+     * 전역 범위 필터는 컨텍스트 외부에서 수행되기 때문에 종속성 주입이 필요한 경우 직접 등록할 수 있음
+     */
+    {
+      provide: 'APP_FILTER',
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
