80793025bdfa0c59add134d96a3aa444008a81d9
Lucid <nohack-@naver.com>
2024-05-16 03:41:03 +0900

    모듈 (Module)

--- 공식 문서/backend/nestjs/src/app.module.ts
+++ 공식 문서/backend/nestjs/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [CatsController],
-  providers: [CatsService],
+  imports: [CatsModule],
 })
 export class AppModule {}
