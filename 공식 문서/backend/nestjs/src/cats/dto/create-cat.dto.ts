e5059468642fb3d7923cabfbb75771ed0ebdb5ef
Lucid <nohack-@naver.com>
2024-05-20 12:48:38 +0900

    Swagger 문서에서 스키마 확인 가능하게 변경

--- 공식 문서/backend/nestjs/src/cats/dto/create-cat.dto.ts
+++ 공식 문서/backend/nestjs/src/cats/dto/create-cat.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsString } from 'class-validator';
 
 export class CreateCatDto {
+  @ApiProperty({
+    description: '고양이 이름',
+  })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({
+    description: '고양이 나이',
+  })
   @IsInt()
   readonly age: number;
 
+  @ApiProperty({
+    description: '고양이 종',
+  })
   @IsString()
   readonly breed: string;
 }
