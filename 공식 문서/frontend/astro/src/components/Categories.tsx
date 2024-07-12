e224410e87b94a40ae2838d3aa48d19035142cdd
Lucid <nohack-@naver.com>
2024-07-12 14:08:45 +0900

    불필요한 코드 제거

--- 공식 문서/frontend/astro/src/components/Categories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type CategoriesProps = {};
-
-const Categories = ({}: CategoriesProps) => {
-  return <div>Categories</div>;
-};
-
-export default Categories;
--- 공식 문서/frontend/astro/src/components/navLiost.ts
+++
@@ -1,7 +0,0 @@
-const navList = [
-  { name: '소개', path: '/about' },
-  { name: '프로젝트', path: '/project' },
-  { name: '스토리', path: '/story' },
-];
-
-export default navList;
