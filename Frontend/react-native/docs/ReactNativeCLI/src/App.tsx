5393651004aac1099e9dbb34870cf1aa0786fbab
Lucid <nohack-@naver.com>
2024-10-11 05:09:41 +0900

    react-native-cli로 프로젝트 설치

---
+++ Frontend/react-native/docs/ReactNativeCLI/src/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default App;
