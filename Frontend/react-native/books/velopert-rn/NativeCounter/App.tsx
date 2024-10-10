4f7f5d31c064bfa80a149f4721e6de58b28c0a82
Lucid <nohack-@naver.com>
2024-10-11 01:40:33 +0900

    네이티브 UI 컴포넌트 사용하기

---
+++ Frontend/react-native/books/velopert-rn/NativeCounter/App.tsx
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Counter from './Counter';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): React.JSX.Element {
+  const [value, setValue] = useState(0);
+
+  return (
+    <Counter
+      style={styles.container}
+      value={value}
+      leftButtonText="+1"
+      rightButtonText="-1"
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
--- Frontend/react-native/books/velopert-rn/NativeCounter/android/app/src/main/java/com/nativecounter/CounterPackage.kt
+++ Frontend/react-native/books/velopert-rn/NativeCounter/android/app/src/main/java/com/nativecounter/CounterPackage.kt
@@ -1,4 +1,19 @@
 package com.nativecounter
 
-class CounterPackage {
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+import java.util.Collections
+
+class CounterPackage : ReactPackage {
+    override fun createNativeModules(p0: ReactApplicationContext): MutableList<NativeModule> {
+        return Collections.emptyList()
+    }
+
+    override fun createViewManagers(p0: ReactApplicationContext): MutableList<ViewManager<*, *>> {
+        val viewManagers = ArrayList<ViewManager<*, *>>()
+        viewManagers.add(CounterManager())
+        return viewManagers
+    }
 }
