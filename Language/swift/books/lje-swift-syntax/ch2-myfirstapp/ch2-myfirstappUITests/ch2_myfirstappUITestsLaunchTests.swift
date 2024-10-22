ef4950e8fbc8402191f78dbcb7141725292095e7
Lucid <nohack-@naver.com>
2024-10-23 00:02:35 +0900

    Xcode와 친해지기

---
+++ Language/swift/books/lje-swift-syntax/ch2-myfirstapp/ch2-myfirstappUITests/ch2_myfirstappUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ch2_myfirstappUITestsLaunchTests.swift
+//  ch2-myfirstappUITests
+//
+//  Created by Lucid on 10/22/24.
+//
+
+import XCTest
+
+final class ch2_myfirstappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Language/swift/books/lje-swift-syntax/ch2-playground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let frame = CGRect(x: 100, y: 100, width: 200, height: 100)
+let view = UILabel(frame: frame)
+
+view.backgroundColor = UIColor.red
+view.textAlignment = .center
+view.text = "Hello, World!!!"
