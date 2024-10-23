180b3bb23209f8ed09eacc94c369a22fd1d8bd39
Lucid <nohack-@naver.com>
2024-10-24 07:51:29 +0900

    블로그 튜토리얼 - Collection 적용

--- 공식 문서/frontend/astro/src/pages/rss.xml.js
+++ 공식 문서/frontend/astro/src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
+
   return rss({
     title: 'Astro Learnen | Blog',
     description: 'Astro Learnen Blog',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.date,
+      link: `/posts/${post.slug}`,
+    })),
     customData: `<language>ko</language>`,
     trailingSlash: false,
   });
