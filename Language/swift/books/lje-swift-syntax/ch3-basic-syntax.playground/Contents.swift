d5e7dd43b2a7f4a7b082f15e3a86866fb87207a5
Lucid <nohack-@naver.com>
2024-10-24 04:51:12 +0900

    기본 문법 : 이것이 바로 스위프트

---
+++ Language/swift/books/lje-swift-syntax/ch3-basic-syntax.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+print(Int.min)
+print(Int.max)
+print(pow(2, 64))
+
+let a = 9
+let b = 2
+
+a / 5
+a % 5
+
+// 닫힌 범위 연산자(Closed Range Operator)
+for i in 1...5 {
+    i
+}
+
+// 반 닫힌 범위 연산자(Half-closed Range Operator)
+for i in 1..<5 {
+    i
+}
