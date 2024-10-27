d67c55d34fe10b22292c54b17449b231926d182e
Lucid <nohack-@naver.com>
2024-10-28 01:40:28 +0900

    Devtools

--- 공식 문서/frontend/tanstack-query/src/App.tsx
+++ 공식 문서/frontend/tanstack-query/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} errorTypes={[]} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
