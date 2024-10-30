9df2d70621c77f101df8277ed03c4b90f1e27340
Lucid <nohack-@naver.com>
2024-10-30 19:34:57 +0900

    스타일링

--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section02/src/pages/index.tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section02/src/pages/index.tsx
@@ -1,4 +1,13 @@
+// _app.tsx를 제외하고는 각 페이지 및 컴포넌트에서의 이름 충돌 문제로 글로벌 CSS를 불러올 수 없다.
+// 이를 해결하기 위해서는 컴포넌트나 페이지에 따라 독립적인 클래스명을 생성하는 CSS Module을 이용할 수 있다.
+import styles from './index.module.css';
+
 // 개발 모드로 켰을 때는, 프리페칭이 동작하지 않는다.
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={`${styles.h1}`}>인덱스</h1>
+      <h2 className={`${styles.h2}`}>H2</h2>
+    </>
+  );
 }
