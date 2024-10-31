a3c2ec2d89bb91b19d9f0499dad34a9abfadca15
Lucid <nohack-@naver.com>
2024-10-31 09:53:04 +0900

    SSG 3. 동적 경로에 적용하기

--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section02/src/pages/book/[id].tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section02/src/pages/book/[id].tsx
@@ -1,8 +1,27 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 import styles from './[id].module.css';
 import fetchOneBook from '@/lib/fetch-one-book';
 
-export const getServerSideProps = (async (context) => {
+export const getStaticPaths = (async () => {
+  return {
+    // URL Param의 값은 프레임워크에서 잘 읽을 수 있도록 문자열로 설정하기
+    paths: [
+      {
+        params: { id: '1' },
+      },
+      {
+        params: { id: '2' },
+      },
+      {
+        params: { id: '3' },
+      },
+    ],
+    // false일 시 NotFound 페이지 렌더링
+    fallback: false,
+  };
+}) satisfies GetStaticPaths;
+
+export const getStaticProps = (async (context) => {
   const { id } = context.params!;
   const book = await fetchOneBook(Number(id));
 
@@ -11,9 +30,20 @@ export const getServerSideProps = (async (context) => {
       book,
     },
   };
-}) satisfies GetServerSideProps;
+}) satisfies GetStaticProps;
 
-export default function Page({ book }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+// export const getServerSideProps = (async (context) => {
+//   const { id } = context.params!;
+//   const book = await fetchOneBook(Number(id));
+
+//   return {
+//     props: {
+//       book,
+//     },
+//   };
+// }) satisfies GetServerSideProps;
+
+export default function Page({ book }: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) return '문제가 발생했습니다. 다시 시도하세요.';
 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
