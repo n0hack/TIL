860b15c9d8d8f2891b294fa5cee4a137afb8b835
Lucid <nohack-@naver.com>
2024-10-31 18:53:10 +0900

    CSS Styling

--- 공식 문서/frontend/nextjs-dashboard-tutorial/app/layout.tsx
+++ 공식 문서/frontend/nextjs-dashboard-tutorial/app/layout.tsx
@@ -1,8 +1,6 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
--- 공식 문서/frontend/nextjs-dashboard-tutorial/app/ui/invoices/status.tsx
+++ 공식 문서/frontend/nextjs-dashboard-tutorial/app/ui/invoices/status.tsx
@@ -4,13 +4,10 @@ import clsx from 'clsx';
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
-        {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-        },
-      )}
+      className={clsx('inline-flex items-center rounded-full px-2 py-1 text-xs', {
+        'bg-gray-100 text-gray-500': status === 'pending',
+        'bg-green-500 text-white': status === 'paid',
+      })}
     >
       {status === 'pending' ? (
         <>
