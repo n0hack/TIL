e8564dd5fdf613dbaf04942037b5dd84a220304a
Lucid <nohack-@naver.com>
2024-10-31 19:01:32 +0900

    Optimizing Fonts and Images

--- 공식 문서/frontend/nextjs-dashboard-tutorial/app/layout.tsx
+++ 공식 문서/frontend/nextjs-dashboard-tutorial/app/layout.tsx
@@ -1,9 +1,10 @@
 import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
