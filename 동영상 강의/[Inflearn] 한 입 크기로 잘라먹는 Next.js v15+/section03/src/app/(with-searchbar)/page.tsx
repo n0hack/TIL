c5f74ffcf10ded5fb1e632fadb2c9a36584656ab
Lucid <nohack-@naver.com>
2024-11-01 14:14:24 +0900

    리액트 서버 컴포넌트 주의사항

--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/page.tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,18 @@
+import { ClientComponent } from './client-component';
 import styles from './page.module.css';
+import { ServerComponent } from './server-component';
 
 // Route Group(소괄호로 감싼 폴더명)을 사용하면, 특정 레이아웃을 원하는 그룹의 페이지에만 적용할 수 있다.
 // 앱 라우터는 컴포넌트가 기본적으로 서버 컴포넌트로 만들어지기 때문에, 번들에 포함되지 않아 TTI 속도가 빨라진다.
 // ㄴ 페이지 라우터에서는 이를 구분하지 못해, 상호작용이 필요 없는 컴포넌트라 하더라도 번들에 포함되었다.
+// 함수는 직렬화가 되지 않기에, 서버 컴포넌트는 Props를 통해 클라이언트 컴포넌트에게 데이터를 전달할 수 없다.
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/searchbar.tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/searchbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 // 클라이언트 컴포넌트는 번들 크기를 줄이기 위해 최대한 적게, 필요한 곳에만 만드는 것이 좋다.
+// 하지만, 클라이언트 컴포넌트도 최초에는 서버에서 사전 렌더링을 위해 한 번 실행되므로 주의가 필요하다.
 const Searchbar = () => {
   const [search, setSearch] = useState('');
 
