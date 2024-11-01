0ccf391317f55471a0a955d1de5081a8dd7a5f61
Lucid <nohack-@naver.com>
2024-11-01 14:57:14 +0900

    한입북스 UI 구현하기

--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/page.tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,6 @@
-import { ClientComponent } from '../../components/client-component';
+import { BookItem } from '@/components/book-item';
 import styles from './page.module.css';
-import { ServerComponent } from '../../components/server-component';
+import books from '@/mock/books.json';
 
 // Route Group(소괄호로 감싼 폴더명)을 사용하면, 특정 레이아웃을 원하는 그룹의 페이지에만 적용할 수 있다.
 // 앱 라우터는 컴포넌트가 기본적으로 서버 컴포넌트로 만들어지기 때문에, 번들에 포함되지 않아 TTI 속도가 빨라진다.
@@ -10,11 +10,19 @@ import { ServerComponent } from '../../components/server-component';
 // 클라이언트는 페이지 교체 시 번들을 토대로 JS를 실행하고, RSC Payload도 적절히 참조하여 페이지 교체를 진행한다.
 export default function Home() {
   return (
-    <div className={styles.page}>
-      인덱스 페이지
-      <ClientComponent>
-        <ServerComponent />
-      </ClientComponent>
+    <div className={styles.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
     </div>
   );
 }
--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/search/page.tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,5 @@
-import { ClientComponent } from '@/components/client-component';
+import { BookItem } from '@/components/book-item';
+import books from '@/mock/books.json';
 
 // 기본적으로 SSG로 생성되나, query나 param을 사용하는 등의 정적 페이지로 만들기 애매한 페이지는 동적인 페이지로 생성된다.
 // SSG는 프리페칭 시 RSC Payload와 번들 모두 불러오지만, Dynamic Page의 경우 RSC Payload만 먼저 불러온다. 번들은 실제 이동 후 불러온다.
@@ -7,11 +8,9 @@ const SearchPage = async ({ searchParams }: { searchParams: Promise<{ q: string 
 
   return (
     <div>
-      SearchPage - {q}
-      {/* 클라이언트 컴포넌트를 포함하면, 네트워크 탭에서 확인 시 rsc 외에 [페이지명].js라는 번들 결과도 확인 가능하다. */}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 };
