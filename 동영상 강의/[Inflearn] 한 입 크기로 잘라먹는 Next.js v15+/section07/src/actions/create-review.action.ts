619f67e45cf9ae9665fd47dc0c036ebbad25d672
Lucid <nohack-@naver.com>
2024-11-07 15:52:15 +0900

    클라이언트 컴포넌트에서의 서버액션

--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section07/src/actions/create-review.action.ts
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section07/src/actions/create-review.action.ts
@@ -5,13 +5,16 @@ import { revalidatePath, revalidateTag } from 'next/cache';
 // 서버 액션을 만들게 되면, 이 코드를 실행하는 API가 자동으로 생성되며 브라우저에서 사용 시 호출된다.
 // 단순한 기능만 처리해도 될 경우에는 간결하게 서버 액션을 만들어 활용하면 좋다.
 // 조금 더 간결하게 편리하게 서버 측 동작을 정의하는 것에 목적을 두고 있다.
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요',
+    };
   }
 
   try {
@@ -19,6 +22,11 @@ export async function createReviewAction(formData: FormData) {
       method: 'POST',
       body: JSON.stringify({ bookId, content, author }),
     });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
     console.log(response.status);
     // 인수로 들어온 페이지 재검증(재생성)하는 함수로 오직 서버 및 서버 액션에서만 실행 가능하다.
     // 해당 경로의 페이지에 있는 모든 서버 로직을 다시 실행하여 페이지를 재생성하게 된다.
@@ -39,7 +47,15 @@ export async function createReviewAction(formData: FormData) {
     // 5. 태그 기준. 데이터 캐시 재검증 (next 옵션 안에 있는 tags)
     // 모든 캐시를 초기화하는 방법보다 효율적이다.
     revalidateTag(`review-${bookId}`);
+
+    return {
+      status: true,
+      error: '',
+    };
   } catch (err) {
-    console.error(err);
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다: ${err}`,
+    };
   }
 }
--- 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section07/src/components/review-editor.tsx
+++ 동영상 강의/[Inflearn] 한 입 크기로 잘라먹는 Next.js v15+/section07/src/components/review-editor.tsx
@@ -1,16 +1,29 @@
+'use client';
+
 import { createReviewAction } from '@/actions/create-review.action';
 import styles from './review-editor.module.css';
+import { useActionState, useEffect } from 'react';
 
 export function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(createReviewAction, null);
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={styles.form_container} action={createReviewAction}>
+      <form className={styles.form_container} action={formAction}>
         {/* 고정적인 값이 필요한 경우, 이런 형태로 트릭을 활용할 수 있다. */}
         <input type="text" name="bookId" value={bookId} hidden readOnly />
-        <textarea name="content" placeholder="리뷰 내용" required />
+        <textarea name="content" placeholder="리뷰 내용" required disabled={isPending} />
         <div className={styles.submit_container}>
-          <input type="text" name="author" placeholder="작성자" required />
-          <button type="submit">작성하기</button>
+          <input type="text" name="author" placeholder="작성자" required disabled={isPending} />
+          <button type="submit" disabled={isPending}>
+            {isPending ? '...' : '작성하기'}
+          </button>
         </div>
       </form>
     </section>
