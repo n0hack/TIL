ba3b3a77979c6591c20829e70ccb0e847d15ad8f
Lucid Jeon <nohack-@naver.com>
2025-02-09 21:39:59 +0900

    React Query 개발자 도구

--- 동영상 강의/udemy-tanstack-query-v5/base-blog-em/src/App.tsx
+++ 동영상 강의/udemy-tanstack-query-v5/base-blog-em/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { Posts } from './Posts';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
