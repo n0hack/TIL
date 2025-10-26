2761b31f4dbfb366ad996b160f6f757b7b28b6e1
Lucid Jeon <nohack-@naver.com>
2025-10-27 05:31:52 +0900

    Validation (검증)

--- 공식 문서/backend/nestjs/techniques/src/main.ts
+++ 공식 문서/backend/nestjs/techniques/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 에러 메시지를 표시하지 않도록 설정
+      // 개발 환경에서만 활성화
+      disableErrorMessages: true,
+
+      // DTO에 없는 값 제거
+      whitelist: true,
+
+      // DTO에 없는 값 오면 에러 발생
+      forbidNonWhitelisted: true,
+
+      // DTO에 맞게 타입 변환
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- 공식 문서/backend/nestjs/techniques/src/user/user.controller.ts
+++ 공식 문서/backend/nestjs/techniques/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return 'This action adds a new user';
+  }
+
+  @Get(':id')
+  // transform에 의해 number 자료형으로 변환
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return `This action returns a #${id} user`;
+  }
+}
