d1090e56b60942bc5d39adb31f7e7b3f3e1a66fe
Lucid (NoHack) <nohack.dev@gmail.com>
2025-11-12 17:09:57 +0900

    Components: Image

--- 공식 문서/frontend/nextjs/next.config.ts
+++ 공식 문서/frontend/nextjs/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://i.namu.wiki/**")],
+  },
 };
 
 export default nextConfig;
